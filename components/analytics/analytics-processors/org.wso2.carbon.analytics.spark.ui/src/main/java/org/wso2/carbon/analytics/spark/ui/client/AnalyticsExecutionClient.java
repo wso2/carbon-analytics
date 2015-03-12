ef79ef8a64f78828d2fda90c3a263e796ed189bf
sinthuja <sinthuja@wso2.com>
2015-03-12 12:04:11 +0530

    Adding spark scripts UI related fixes.

---
+++ components/analytics/analytics-processors/org.wso2.carbon.analytics.spark.ui/src/main/java/org/wso2/carbon/analytics/spark/ui/client/AnalyticsExecutionClient.java
@@ -0,0 +1,137 @@
+/*
+*  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.carbon.analytics.spark.ui.client;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.client.Options;
+import org.apache.axis2.client.ServiceClient;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.transport.http.HTTPConstants;
+import org.wso2.carbon.analytics.spark.admin.stub.AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException;
+import org.wso2.carbon.analytics.spark.admin.stub.AnalyticsProcessorAdminServiceStub;
+
+import java.rmi.RemoteException;
+
+public class AnalyticsExecutionClient {
+    private AnalyticsProcessorAdminServiceStub stub;
+
+    public AnalyticsExecutionClient(String cookie,
+                                    String backEndServerURL,
+                                    ConfigurationContext configCtx) throws AxisFault {
+        String serviceURL = backEndServerURL + "AnalyticsProcessorAdminService";
+        stub = new AnalyticsProcessorAdminServiceStub(configCtx, serviceURL);
+        ServiceClient client = stub._getServiceClient();
+        int timeout = 60 * 60 * 1000; // 1 hour
+        stub._getServiceClient().getOptions().setTimeOutInMilliSeconds(timeout);
+        stub._getServiceClient().getOptions().setProperty(
+                HTTPConstants.SO_TIMEOUT, timeout);
+        stub._getServiceClient().getOptions().setProperty(
+                HTTPConstants.CONNECTION_TIMEOUT, timeout);
+        Options option = client.getOptions();
+        option.setManageSession(true);
+        option.setTimeOutInMilliSeconds(timeout);
+        option.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, cookie);
+    }
+
+    public String execute(String query) throws RemoteException, AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException {
+        String resultString;
+        AnalyticsProcessorAdminServiceStub.AnalyticsQueryResultDto queryResult = stub.executeQuery(query);
+        resultString = JsonResult(query, queryResult);
+        return resultString;
+    }
+
+    public void executeScriptContent(String scriptContent) throws RemoteException, AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException {
+        AnalyticsProcessorAdminServiceStub.AnalyticsQueryResultDto[] queryResult = stub.execute(scriptContent);
+    }
+
+    public AnalyticsProcessorAdminServiceStub.AnalyticsQueryResultDto[] executeScript(String scriptName) throws RemoteException, AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException {
+        return stub.executeScript(scriptName);
+    }
+
+    public void deleteScript(String scriptName) throws RemoteException, AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException {
+        stub.deleteScript(scriptName);
+    }
+
+    public void saveScript(String scriptName, String scriptContent, String cron) throws RemoteException, AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException {
+        stub.saveScript(scriptName, scriptContent, cron);
+    }
+
+    public String[] getAllScripts() throws RemoteException {
+        return stub.getAllScriptNames();
+    }
+
+    public AnalyticsProcessorAdminServiceStub.AnalyticsScriptDto getScriptContent(String scriptName) throws RemoteException, AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException {
+        return stub.getScript(scriptName);
+    }
+
+    public void updateScript(String scriptName, String scriptContent, String cron) throws
+            RemoteException, AnalyticsProcessorAdminServiceAnalyticsProcessorAdminExceptionException {
+        if (scriptContent != null) {
+            stub.updateScriptContent(scriptName, scriptContent);
+        }
+        if (cron != null) {
+            stub.updateScriptTask(scriptName, cron);
+        }
+    }
+
+    private String JsonResult(String query, AnalyticsProcessorAdminServiceStub.AnalyticsQueryResultDto res) {
+        JsonObject resObj = new JsonObject();
+
+        JsonObject meta = new JsonObject();
+        meta.addProperty("code", 200);
+        meta.addProperty("responseMessage", "EXECUTED QUERY : " + query);
+        JsonArray colArray = new JsonArray();
+
+        if (res != null) {
+            for (String col : res.getColumnNames()) {
+                colArray.add(new JsonPrimitive(col));
+            }
+        }
+        meta.add("columns", colArray);
+        resObj.add("meta", meta);
+
+        JsonObject response = new JsonObject();
+        JsonArray rows = new JsonArray();
+
+        if (res != null) {
+            for (AnalyticsProcessorAdminServiceStub.AnalyticsRowResultDto row : res.getRowsResults()) {
+                JsonArray singleRow = new JsonArray();
+                int i = 0;
+                for (String elm : row.getColumnValues()) {
+                    if (elm != null) {
+                        singleRow.add(new JsonPrimitive(elm.toString()));
+                    } else {
+                        singleRow.add(new JsonPrimitive("NULL"));
+                    }
+                    i++;
+                }
+                rows.add(singleRow);
+            }
+        }
+
+        response.add("items", rows);
+        resObj.add("response", response);
+
+        return resObj.toString();
+    }
+
+
+}
---
+++ components/analytics/analytics-processors/org.wso2.carbon.analytics.spark.ui/src/main/resources/web/spark-management/index.jsp
@@ -0,0 +1,135 @@
+<%@ page import="org.apache.axis2.context.ConfigurationContext" %>
+<%@ page import="org.wso2.carbon.CarbonConstants" %>
+<%@ page import="org.wso2.carbon.ui.CarbonUIUtil" %>
+<%@ page import="org.wso2.carbon.utils.ServerConstants" %>
+<!--
+~ Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+~
+~ WSO2 Inc. licenses this file to you under the Apache License,
+~ Version 2.0 (the "License"); you may not use this file except
+~ in compliance with the License.
+~ You may obtain a copy of the License at
+~
+~ http://www.apache.org/licenses/LICENSE-2.0
+~
+~ Unless required by applicable law or agreed to in writing,
+~ software distributed under the License is distributed on an
+~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+~ KIND, either express or implied. See the License for the
+~ specific language governing permissions and limitations
+~ under the License.
+-->
+
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib uri="http://wso2.org/projects/carbon/taglibs/carbontags.jar" prefix="carbon" %>
+
+<fmt:bundle basename="org.wso2.carbon.analytics.spark.ui.i18n.Resources">
+    <carbon:breadcrumb label="spark.conosle.menu"
+                       resourceBundle="org.wso2.carbon.analytics.spark.ui.i18n.Resources"
+                       topPage="true" request="<%=request%>"/>
+
+    <link type="text/css" href="css/main.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="css/Ptty.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/Ptty.jquery.js"></script>
+
+    <div id="middle">
+        <h2>Interactive Spark Console</h2>
+
+        <div id="workArea">
+
+            <div id="terminalArea">
+                <section>
+                    <div id="terminal"></div>
+                </section>
+
+                <script>
+                    $(document).ready(function () {
+                        /* Start Ptty terminal */
+                        $('#terminal').Ptty();
+
+                        $.register_command(
+                                'define',
+                                'Defines the schema of a table',
+                                'DEFINE TABLE [table]',
+                                '../spark-console/execute_spark_ajaxprocessor.jsp'
+                        );
+
+                        $.register_command(
+                                'select',
+                                'Execute a Spark SELECT query',
+                                'SELECT [query]',
+                                '../spark-console/execute_spark_ajaxprocessor.jsp'
+                        );
+
+                        $.register_command(
+                                'insert',
+                                'Inserts data into tables',
+                                'INSERT INTO [query]',
+                                '../spark-console/execute_spark_ajaxprocessor.jsp'
+                        );
+
+                        $.register_command(
+                                'about',
+                                'Interactive Spark SQL shell',
+                                'about [no options]',
+                                function () {
+                                    var about = '<p>This interactive shell lets you execute Spark SQL commands against a local Spark cluster.<br>' +
+                                            'Running on Spark v 1.2.1</p>';
+
+                                    return {
+                                        type: 'print',
+                                        out: about
+                                    };
+                                }
+                        );
+
+                        /* Register Commands and Callbacks*/
+                        $.register_command(
+                                'date',
+                                'returns the current date',
+                                'date [no options]',
+                                function () {
+                                    var now = new Date();
+                                    return {
+                                        type: 'print',
+                                        out: now + ' '
+                                    };
+                                }
+                        );
+
+                        // Typewriter effect callback
+                        $.register_callback('typewriter', function (data) {
+                            var text_input = $('.cmd_terminal_prompt');
+                            text_input.hide();
+                            if (typeof data.write === 'string') {
+                                // decode special entities.
+                                var str = $('<div/>').html(data.write + ' ').text(),
+                                        typebox = $('<div></div>').appendTo('.cmd_terminal_content'),
+                                        i = 0,
+                                        isTag,
+                                        text;
+                                (function typewriter() {
+                                    text = str.slice(0, ++i);
+                                    if (text === str) return text_input.show();
+
+                                    typebox.html(text);
+
+                                    var char = text.slice(-1);
+                                    if (char === '<') isTag = true;
+                                    if (char === '>') isTag = false;
+
+                                    if (isTag) return typewriter();
+                                    setTimeout(typewriter, 40);
+                                }());
+                            }
+                        });
+
+                    });
+                </script>
+            </div>
+        </div>
+    </div>
+</fmt:bundle>
