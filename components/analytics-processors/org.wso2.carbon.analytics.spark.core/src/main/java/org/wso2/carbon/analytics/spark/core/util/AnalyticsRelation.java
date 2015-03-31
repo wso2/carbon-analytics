b46bc12ce55e8f0c2213018103aad0bc0bf01282
niranda <niranda.perera@gmail.com>
2015-03-31 13:59:12 +0530

    initial commit - migration mid way

--- components/analytics-processors/org.wso2.carbon.analytics.spark.core/src/main/java/org/wso2/carbon/analytics/spark/core/util/AnalyticsRelation.java
+++ components/analytics-processors/org.wso2.carbon.analytics.spark.core/src/main/java/org/wso2/carbon/analytics/spark/core/util/AnalyticsRelation.java
@@ -22,6 +22,8 @@ import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.sources.BaseRelation;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.sources.TableScan;
 import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.DataTypes;
