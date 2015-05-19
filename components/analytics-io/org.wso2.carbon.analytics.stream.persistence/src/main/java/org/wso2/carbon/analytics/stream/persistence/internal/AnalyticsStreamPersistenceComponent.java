8c356551b0f56830675357caea3cab8d9bdac8a9
wggihan <wggihan@gmail.com>
2015-05-19 11:45:27 +0530

    Add stream persistence back end changes

---
+++ components/analytics-io/org.wso2.carbon.analytics.stream.persistence/src/main/java/org/wso2/carbon/analytics/stream/persistence/internal/AnalyticsStreamPersistenceComponent.java
@@ -0,0 +1,49 @@
+/*
+* Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+package org.wso2.carbon.analytics.stream.persistence.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.analytics.dataservice.AnalyticsDataService;
+
+/**
+ * This class represents the analytics stream persistence service declarative services component.
+ *
+ * @scr.component name="analytics.stream.persistence.component" immediate="true"
+ * @scr.reference name="analytics.component" interface="org.wso2.carbon.analytics.dataservice.AnalyticsDataService"
+ * cardinality="1..1" policy="dynamic" bind="setAnalyticsDataService" unbind="unsetAnalyticsDataService"
+ */
+public class AnalyticsStreamPersistenceComponent {
+
+    private static final Log logger = LogFactory.getLog(AnalyticsStreamPersistenceComponent.class);
+
+    protected void setAnalyticsDataService(AnalyticsDataService analyticsDataService) {
+        if (logger.isDebugEnabled()) {
+            logger.info("Setting the Analytics Data Service");
+        }
+        ServiceHolder.setAnalyticsDataService(analyticsDataService);
+    }
+
+    protected void unsetAnalyticsDataService(AnalyticsDataService analyticsDataService) {
+        if (logger.isDebugEnabled()) {
+            logger.info("Unsetting the Analytics Data Service");
+        }
+        ServiceHolder.setAnalyticsDataService(null);
+    }
+}
