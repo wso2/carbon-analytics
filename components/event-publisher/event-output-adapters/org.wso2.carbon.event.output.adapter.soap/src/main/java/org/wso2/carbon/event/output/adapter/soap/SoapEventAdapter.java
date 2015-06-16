6c4d074a42dd35fd24e96e8a15e7c82c1e4c78b2
Tharik Kanaka <onestepfromhell@hotmail.com>
2015-06-16 09:36:49 +0530

    Copyright license   updated

--- components/event-publisher/event-output-adapters/org.wso2.carbon.event.output.adapter.soap/src/main/java/org/wso2/carbon/event/output/adapter/soap/SoapEventAdapter.java
+++ components/event-publisher/event-output-adapters/org.wso2.carbon.event.output.adapter.soap/src/main/java/org/wso2/carbon/event/output/adapter/soap/SoapEventAdapter.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.wso2.carbon.event.output.adapter.soap;
 
 import org.apache.axiom.om.OMElement;
