d4491ffd5d8c4c2d081476780d14797aab92021b
niranda <niranda.perera@gmail.com>
2015-07-05 16:41:07 +0530

    analytics persistence mgr error - midway tests fixed

--- components/analytics-processors/org.wso2.carbon.analytics.spark.core/src/test/java/org/wso2/carbon/analytics/spark/core/internal/AnalyticsSparkExecutorTest.java
+++ components/analytics-processors/org.wso2.carbon.analytics.spark.core/src/test/java/org/wso2/carbon/analytics/spark/core/internal/AnalyticsSparkExecutorTest.java
@@ -316,6 +316,7 @@ public class AnalyticsSparkExecutorTest {
         System.setProperty(AnalyticsServiceHolder.FORCE_INDEXING_ENV_PROP, Boolean.TRUE.toString());
         this.service = ServiceHolder.getAnalyticsDataService();
         ServiceHolder.setAnalyticskExecutor(new SparkAnalyticsExecutor("localhost", 0, "src/test/resources/conf1"));
+        ServiceHolder.getAnalyticskExecutor().startSparkServer("src/test/resources/conf1");
     }
 
     @AfterClass
