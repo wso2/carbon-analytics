2ac97cccbae49aa5ebee3b45fac9b6bb422a95cb
Lasantha Fernando <lasantha.fdo@gmail.com>
2015-07-10 19:46:28 +0530

    Fix for https://wso2.org/jira/browse/CEP-1221; Fixed typo in log message

--- components/data-bridge/org.wso2.carbon.databridge.receiver.thrift/src/main/java/org/wso2/carbon/databridge/receiver/thrift/converter/ThriftEventConverter.java
+++ components/data-bridge/org.wso2.carbon.databridge.receiver.thrift/src/main/java/org/wso2/carbon/databridge/receiver/thrift/converter/ThriftEventConverter.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.wso2.carbon.databridge.receiver.thrift.converter;
 
@@ -89,7 +89,7 @@ public final class ThriftEventConverter implements EventConverter {
         if (eventBundle instanceof ThriftEventBundle) {
             return createEventList((ThriftEventBundle) eventBundle, streamTypeHolder);
         } else {
-            throw new EventConversionException("Wrong type event relieved " + eventBundle.getClass());
+            throw new EventConversionException("Wrong type of event received " + eventBundle.getClass());
         }
 
     }
