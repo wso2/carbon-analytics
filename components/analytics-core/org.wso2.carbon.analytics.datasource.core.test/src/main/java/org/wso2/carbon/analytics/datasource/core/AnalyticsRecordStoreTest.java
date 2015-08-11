29079b2fcaf1a9121f1b047f2985985ec9ed4b6b
Anjana Fernando <lafernando@gmail.com>
2015-08-12 03:05:23 +0530

    Cleanup

--- components/analytics-core/org.wso2.carbon.analytics.datasource.core.test/src/main/java/org/wso2/carbon/analytics/datasource/core/AnalyticsRecordStoreTest.java
+++ components/analytics-core/org.wso2.carbon.analytics.datasource.core.test/src/main/java/org/wso2/carbon/analytics/datasource/core/AnalyticsRecordStoreTest.java
@@ -35,8 +35,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.text.TabExpander;
-
 /**
  * This class contains tests related to {@link AnalyticsRecordStore}.
  */
