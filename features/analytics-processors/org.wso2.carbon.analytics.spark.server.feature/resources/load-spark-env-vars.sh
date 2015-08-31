fa31f48a243090dbf7e1356f9601956f6c3861f6
niranda <niranda.perera@gmail.com>
2015-08-31 10:31:04 +0530

    fixing DAS 96 - cleaning up

--- features/analytics-processors/org.wso2.carbon.analytics.spark.server.feature/resources/load-spark-env-vars.sh
+++ features/analytics-processors/org.wso2.carbon.analytics.spark.server.feature/resources/load-spark-env-vars.sh
@@ -5,6 +5,6 @@ echo 'Loading spark environment variables '
 export CARBON_SPARK_HOME=$CARBON_HOME
 export _SPARK_ASSEMBLY=$CARBON_SPARK_HOME/repository/components/plugins/spark-core_2.10_*.wso2*.jar
 export SPARK_SCALA_VERSION=2.10
-export SPARK_CLASSPATH=`java -cp $CARBON_SPARK_HOM/repository/components/plugins/org.wso2.carbon.analytics.spark.utils*.jar org.wso2.carbon.analytics.spark.utils.ComputeClasspath $CARBON_HOME`
-export SPARK_CLASSPATH=$SPARK_CLASSPATH:$(echo $CARBON_SPARK_HOM/repository/components/lib/*.jar | tr ' ' ':')
+export SPARK_CLASSPATH=`java -cp $CARBON_SPARK_HOME/repository/components/plugins/org.wso2.carbon.analytics.spark.utils*.jar org.wso2.carbon.analytics.spark.utils.ComputeClasspath $CARBON_HOME`
+export SPARK_CLASSPATH=$SPARK_CLASSPATH:$(echo $CARBON_SPARK_HOME/repository/components/lib/*.jar | tr ' ' ':')
 mkdir -p $CARBON_SPARK_HOME/lib_managed/jars
