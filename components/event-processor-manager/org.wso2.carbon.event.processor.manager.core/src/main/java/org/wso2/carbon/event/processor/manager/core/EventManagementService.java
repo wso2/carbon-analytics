ce2c4954ef6fd89b6bc7877032f7b95c4caf5895
ramindu90 <ramindus@wso2.com>
2015-09-03 15:15:43 +0530

    Adding doc comments

--- components/event-processor-manager/org.wso2.carbon.event.processor.manager.core/src/main/java/org/wso2/carbon/event/processor/manager/core/EventManagementService.java
+++ components/event-processor-manager/org.wso2.carbon.event.processor.manager.core/src/main/java/org/wso2/carbon/event/processor/manager/core/EventManagementService.java
@@ -23,22 +23,66 @@ import org.wso2.siddhi.core.event.Event;
 
 public interface EventManagementService {
 
+    /**
+     * This method returns the {@link ManagementModeInfo} which contains the node type and its configurations
+     * @return {@link ManagementModeInfo}
+     */
     public ManagementModeInfo getManagementModeInfo();
 
+    /**
+     * This method specifies the node type
+     * @param manager   manager node can be either a {@link EventProcessorManagementService},
+     *                  {@link EventReceiverManagementService} or {@link EventPublisherManagementService}
+     */
     public void subscribe(Manager manager);
 
+    /**
+     * This method unsubscribes the node according to the {@link Manager} type
+     * @param manager   manager node can be either a {@link EventProcessorManagementService},
+     *                  {@link EventReceiverManagementService} or {@link EventPublisherManagementService}
+     */
     public void unsubscribe(Manager manager);
 
+    /**
+     * This method syncs a particular event with the other node. Depending on the node type and address, the events
+     * will be sent
+     * @param syncId    stream id for which StreamRuntimeInfo needed in the StreamRuntimeInfo map
+     * @param type      node type
+     * @param event     the event which is syncing with the other node
+     */
     public void syncEvent(String syncId, Manager.ManagerType type, Event event);
 
+
+    /**
+     * This method registers the events in the EventPublisher which needed to be synced
+     * @param eventSync contains the stream id
+     */
     public void registerEventSync(EventSync eventSync);
 
+    /**
+     * This method unregisters the events in the EventPublisher which scheduled to be synced
+     * @param syncId id of the events to be synced by the EventPublisher
+     */
     public void unregisterEventSync(String syncId);
 
+    /**
+     * This method updates the hazelcast map with the latest processed event details
+     * @param publisherName event publisher name
+     * @param tenantId      tenant id
+     * @param timestamp     hazelcast cluster time
+     */
     public void updateLatestEventSentTime(String publisherName, int tenantId, long timestamp);
 
+    /**
+     * This method gets the latest event processed time
+     * @param publisherName event publisher name
+     * @param tenantId      tenant id
+     */
     public long getLatestEventSentTime(String publisherName, int tenantId);
 
+    /**
+     * This method return hazelcast cluster time
+     */
     public long getClusterTimeInMillis();
 
 }
