29b32e34690e55e99b1f1774408c159be01420e4
thiliA <thilinia@wso2.com>
2015-09-25 12:18:03 +0530

    Fixing exception handling in InputAdapterRuntime destroy method and removing unnecessary new lines

--- components/event-receiver/org.wso2.carbon.event.input.adapter.core/src/main/java/org/wso2/carbon/event/input/adapter/core/internal/InputAdapterRuntime.java
+++ components/event-receiver/org.wso2.carbon.event.input.adapter.core/src/main/java/org/wso2/carbon/event/input/adapter/core/internal/InputAdapterRuntime.java
@@ -1,16 +1,19 @@
 /*
- * Copyright (c) 2005 - 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.event.input.adapter.core.internal;
@@ -87,14 +90,12 @@ public class InputAdapterRuntime implements InputEventAdapterListener {
     public void destroy() {
         if (inputEventAdapter != null) {
             try {
-                try {
-                    inputEventAdapter.disconnect();
-                } finally {
-                    inputEventAdapter.destroy();
-                }
+                inputEventAdapter.disconnect();
             } catch (Throwable e) {
-                log.error("Error when destroying Input Adapter '" + name + "'," +
+                log.error("Error when disconnecting Input Adapter '" + name + "'," +
                         e.getMessage(), e);
+            } finally {
+                inputEventAdapter.destroy();
             }
         }
     }
