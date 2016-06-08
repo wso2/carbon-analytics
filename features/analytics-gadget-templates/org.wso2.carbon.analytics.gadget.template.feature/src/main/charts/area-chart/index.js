469c6682d595497a30662bb62f74a0c8eb3224e5
Tharik Kanaka <tharik.kanaka@gmail.com>
2016-06-08 16:51:36 +0530

    area chart improved

--- features/analytics-gadget-templates/org.wso2.carbon.analytics.gadget.template.feature/src/main/charts/area-chart/index.js
+++ features/analytics-gadget-templates/org.wso2.carbon.analytics.gadget.template.feature/src/main/charts/area-chart/index.js
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,8 +28,22 @@ var getConfig, validate, isProviderRequired, draw, update;
         /*
          dynamic logic goes here
          */
-        return chartConf;
 
+        var columns = [];
+
+        columns.push("None");
+        for(var i=0; i < schema.length; i++) {
+            columns.push(schema[i]["fieldName"]);
+        }
+
+        for(var i=0; i < chartConf.length; i++) {
+            if (chartConf[i]["fieldName"] == "color") {
+                chartConf[i]["valueSet"] = columns;
+                break;
+            }
+        }
+
+        return chartConf;
     };
 
     /**
@@ -103,6 +117,12 @@ var getConfig, validate, isProviderRequired, draw, update;
             type : "area",
             y: _chartConfig.y
         };
+
+        if (_chartConfig.color != "None") {
+            conf.charts[0].color = _chartConfig.color;
+            conf.charts[0].mode = _chartConfig.mode;
+        }
+
         return conf;
     };
 
