75a267e00d48943c750d0c6ab0d352fd97efe409
dilini <dilini@wso2.com>
2016-06-10 14:07:27 +0530

    Changing DEPLOYER_META_INFO_PATH

--- components/execution-manager/org.wso2.carbon.event.execution.manager.core/src/main/java/org/wso2/carbon/event/execution/manager/core/internal/util/ExecutionManagerConstants.java
+++ components/execution-manager/org.wso2.carbon.event.execution.manager.core/src/main/java/org/wso2/carbon/event/execution/manager/core/internal/util/ExecutionManagerConstants.java
@@ -36,8 +36,7 @@ public class ExecutionManagerConstants {
     //this is the registry path where specific deployers save their meta information.
     public static final String DEPLOYER_META_INFO_PATH = RegistryConstants.PATH_SEPARATOR + "repository"
                                                          + RegistryConstants.PATH_SEPARATOR + "components" + RegistryConstants.PATH_SEPARATOR
-                                                         + "org.wso2.carbon.event.execution.manager.core" + RegistryConstants.PATH_SEPARATOR
-                                                         + "deployer-meta-info";
+                                                         + "execution.manager.template.deployers";
 
     public static final String CONFIG_NAME_SEPARATOR = "-";
 
