027768c62b74fa6cf186c3e466e650ef82a3e49a
ramindu90 <ramindus@wso2.com>
2016-07-07 19:38:05 +0530

    Adding geo-map template

---
+++ features/analytics-gadget-templates/org.wso2.carbon.analytics.gadget.template.feature/src/main/charts/geo-map/js/map-config.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var floorConfig = [
+    // {
+    //     mapDetails: {
+    //         imageBounds: {
+    //             northEast: {
+    //                 lat: 0.0,
+    //                 long: 0.0
+    //             },
+    //             southWest: {
+    //                 lat: 0.0,
+    //                 long: 0.0
+    //             }
+    //         },
+    //         areaName: "ewr",
+    //         urls: [{url: "lib/img/floors/building-1/floor_1.svg", layerName:"floor-1"},
+    //             {url: "lib/img/floors/building-1/floor_2.svg", layerName:"floor-2"}]
+    //     }
+    // },
+    // {
+    //     mapDetails: {
+    //         imageBounds: {
+    //             northEast: {
+    //                 lat: 0.0,
+    //                 long: 0.0
+    //             },
+    //             southWest: {
+    //                 lat: 0.0,
+    //                 long: 0.0
+    //             }
+    //         },
+    //         areaName: "ord",
+    //         urls: [{url: "lib/img/floors/building-2/floor_1.svg", layerName:"floor-1"},
+    //             {url: "lib/img/floors/building-2/floor_2.svg", layerName:"floor-2"}]
+    //     }
+    // }
+];
