f2a2a211811bfe735d1edadc143dd02bb0080a23
Sachith Withana <swsachith@gmail.com>
2016-10-13 09:33:41 +0530

    adding the initial repo

---
+++ src/main/java/org/wso2/carbon/analytics/App.java
@@ -0,0 +1,13 @@
+package org.wso2.carbon.analytics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
