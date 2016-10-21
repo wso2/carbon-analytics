0affe4a1c2180330b31e8c10b7748d6b14c6c9a2
gokul <royalgok@gmail.com>
2016-10-21 11:43:05 +0530

    Fixing path building bug in external-spark-classpath config

--- components/analytics-processors/org.wso2.carbon.analytics.spark.utils/src/main/java/org/wso2/carbon/analytics/spark/utils/ComputeClasspath.java
+++ components/analytics-processors/org.wso2.carbon.analytics.spark.utils/src/main/java/org/wso2/carbon/analytics/spark/utils/ComputeClasspath.java
@@ -187,7 +187,7 @@ public class ComputeClasspath {
                 } else if (fileExists(carbonHome + File.separator + line)) {
                     buf.append(separator);
                     buf.append(carbonHome);
-                    buf.append(separator);
+                    buf.append(File.separator);
                     buf.append(line);
                 } else {
                     throw new IOException("File not found : " + line);
