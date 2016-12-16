5baefc69e889151bc669307122b605ab5d4b7053
samgregoost <samramasinghe@gmail.com>
2016-12-16 17:01:43 +0530

    added code formatting

--- components/event-publisher/event-output-adapters/org.wso2.carbon.event.output.adapter.rdbms/src/test/java/org/wso2/carbon/event/output/adapter/rdbms/test/RDBMSEventPublusherForNullValuesTestCase.java
+++ components/event-publisher/event-output-adapters/org.wso2.carbon.event.output.adapter.rdbms/src/test/java/org/wso2/carbon/event/output/adapter/rdbms/test/RDBMSEventPublusherForNullValuesTestCase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.event.output.adapter.rdbms.test;
 
 import junit.framework.Assert;
@@ -19,10 +34,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-/**
- * Created by sameera on 12/16/16.
- */
 public class RDBMSEventPublusherForNullValuesTestCase {
 
     private static final String CREATE_TEST_TABLE = "CREATE TABLE IF NOT EXISTS TEST_TABLE (\n" +
