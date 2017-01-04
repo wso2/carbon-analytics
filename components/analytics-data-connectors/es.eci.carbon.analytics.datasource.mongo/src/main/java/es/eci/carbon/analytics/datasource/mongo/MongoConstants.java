033b86ca1b3c757f69d8394d114c237c688bc8d8
Jose Maria Alvarez <josemaria.alvarezfernandez@elcorteingles.es>
2017-01-04 11:07:13 +0100

    First commit to support Mongo. All tests are passing.

---
+++ components/analytics-data-connectors/es.eci.carbon.analytics.datasource.mongo/src/main/java/es/eci/carbon/analytics/datasource/mongo/MongoConstants.java
@@ -0,0 +1,11 @@
+package es.eci.carbon.analytics.datasource.mongo;
+
+public class MongoConstants {
+
+	public static final String DATASOURCE_NAME = "datasource";
+
+	public static final String DATABASE = "database";
+
+	public static final String DEFAULT_ARS_DB_NAME = "ARS";
+
+}
