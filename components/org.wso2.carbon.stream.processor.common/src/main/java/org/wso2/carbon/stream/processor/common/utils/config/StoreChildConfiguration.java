819637c482f958fa7544aa7fe9db7fadcd45a83b
Niveathika <niveathika@wso2.com>
2017-08-03 19:58:25 +0530

    Configurations reader improved to access store element along with elements

--- components/org.wso2.carbon.stream.processor.common/src/main/java/org/wso2/carbon/stream/processor/common/utils/config/StoreChildConfiguration.java
+++ components/org.wso2.carbon.stream.processor.common/src/main/java/org/wso2/carbon/stream/processor/common/utils/config/StoreChildConfiguration.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (c)  2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -23,35 +23,34 @@ import org.wso2.carbon.kernel.annotations.Element;
 import java.util.Map;
 
 /**
- * A second level configuration bean class for siddhi extension config.
+ * A second level configuration bean class for siddhi store config.
  */
-@Configuration(description = "Extensions configuration")
-public class ChildConfiguration {
+@Configuration(description = "Store configuration")
+public class StoreChildConfiguration {
 
-    @Element(description = "Extension name")
+    @Element(description = "Store name")
     private String name = "";
 
-    @Element(description = "Extension namespace")
-    private String namespace = "";
+    @Element(description = "Store type")
+    private String type = "";
 
-    @Element(description = "Extension properties")
+    @Element(description = "Store properties")
     private Map<String, String> properties;
 
     public String getName() {
         return name;
     }
 
+    public String getType() {
+        return type;
+    }
+
     public Map<String, String> getProperties() {
         return properties;
     }
 
-    public String getNamespace() {
-        return namespace;
-    }
-
     @Override
     public String toString() {
-        return "name : " + name + ", namespace : " + namespace + ", properties : " + properties.toString();
+        return "name : " + name + ", properties : " + properties.toString();
     }
 }
-
