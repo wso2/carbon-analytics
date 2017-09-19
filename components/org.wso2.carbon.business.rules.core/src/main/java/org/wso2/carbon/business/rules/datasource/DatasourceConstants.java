e5464d41d3da4d9ee4ed183c754fd2a293bca300
minudika <minudika001@gmail.com>
2017-09-19 23:50:58 +0530

    Generating queries to insert, update, delete and retrieve business rules from/to database.

---
+++ components/org.wso2.carbon.business.rules.core/src/main/java/org/wso2/carbon/business/rules/datasource/DatasourceConstants.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.business.rules.datasource;
+
+public class DatasourceConstants {
+    public static String db_type = "mysql";
+    public static String componentNamespace = "business.rules";
+
+    public static String PRIMARY_KEY = "PRIMARY_KAY";
+    public static String BUSINESS_RULE = "BUSINESS_RULE";
+    public static String DEPLOYMENT_STATUS = "DEPLOYMENT_STATUS";
+
+    public static String INSERT = "INSERT";
+    public static String UPDATE = "UPDATE";
+    public static String READ = "READ";
+    public static String DELETE = "DELETE";
+
+}
