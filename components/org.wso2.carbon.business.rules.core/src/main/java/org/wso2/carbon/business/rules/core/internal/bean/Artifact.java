63da19d1d85180724dcca322b44af740556a839c
senthuran16 <senthuranrc16@gmail.com>
2017-09-20 18:34:54 +0530

    Made class 'Artifact' as a child of class 'Template'

--- components/org.wso2.carbon.business.rules.core/src/main/java/org/wso2/carbon/business/rules/core/internal/bean/Artifact.java
+++ components/org.wso2.carbon.business.rules.core/src/main/java/org/wso2/carbon/business/rules/core/internal/bean/Artifact.java
@@ -22,47 +22,15 @@ package org.wso2.carbon.business.rules.core.internal.bean;
  * Represents an Artifact, which is a derived Template
  * i.e : SiddhiApp (Not considering Gadget & Dashboard for now)
  */
-public class Artifact {
-    private String type;
-    private String content;
-    private String exposedStreamDefinition;
-
+public class Artifact extends Template {
     public Artifact(String type, String content, String exposedStreamDefinition) {
-        this.type = type;
-        this.content = content;
-        this.exposedStreamDefinition = exposedStreamDefinition;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getExposedStreamDefinition() {
-        return exposedStreamDefinition;
-    }
-
-    public void setExposedStreamDefinition(String exposedStreamDefinition) {
-        this.exposedStreamDefinition = exposedStreamDefinition;
+        super(type, content, exposedStreamDefinition);
     }
 
     @Override
     public String toString() {
-        return "Template{" +
-                "\ntype='" + type + '\'' +
-                ",\n content='" + content + '\'' +
-                ",\n exposedStreamDefinition='" + exposedStreamDefinition + '\'' +
-                "\n}";
+        return "Artifact{" +
+                "\n" + super.toString() +
+                "}";
     }
 }
