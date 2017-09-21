9094cf73f347d2db17e787dd354501e97b669203
dnwick <damithn@wso2.com>
2017-09-21 11:56:11 +0530

    Added debug logs for service components activates (#430)
    * fixed issues #52 and #50
    
    * Added debug logs for service components activates

--- components/org.wso2.carbon.event.simulator.core/src/gen/java/org/wso2/carbon/event/simulator/core/api/FilesApi.java
+++ components/org.wso2.carbon.event.simulator.core/src/gen/java/org/wso2/carbon/event/simulator/core/api/FilesApi.java
@@ -1,39 +1,28 @@
 package org.wso2.carbon.event.simulator.core.api;
 
-import io.swagger.annotations.ApiParam;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wso2.carbon.event.simulator.core.api.FilesApiService;
+import org.wso2.carbon.event.simulator.core.api.NotFoundException;
 import org.wso2.carbon.event.simulator.core.factories.FilesApiServiceFactory;
 import org.wso2.carbon.event.simulator.core.internal.util.EventSimulatorConstants;
 import org.wso2.carbon.event.simulator.core.model.*;
-import org.wso2.carbon.event.simulator.core.api.FilesApiService;
-
-
-import java.io.File;
-
 import org.wso2.carbon.event.simulator.core.model.InlineResponse2001;
-
-import java.nio.file.Paths;
-import java.util.List;
-
-import org.wso2.carbon.event.simulator.core.api.NotFoundException;
-
-import java.io.InputStream;
-
 import org.wso2.carbon.event.simulator.core.service.EventSimulatorDataHolder;
 import org.wso2.carbon.event.simulator.core.service.EventSimulatorMap;
 import org.wso2.carbon.utils.Utils;
 import org.wso2.msf4j.Microservice;
-import org.wso2.msf4j.formparam.FormDataParam;
 import org.wso2.msf4j.formparam.FileInfo;
+import org.wso2.msf4j.formparam.FormDataParam;
 
-import javax.ws.rs.core.Context;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
-import javax.ws.rs.*;
+
+import io.swagger.annotations.ApiParam;
 
 
 @Component(
@@ -132,8 +121,10 @@ public class FilesApi implements Microservice {
         //set maximum csv file size to 8MB
         EventSimulatorDataHolder.getInstance().setMaximumFileSize(8388608);
         EventSimulatorDataHolder.getInstance().setCsvFileDirectory(Paths.get(Utils.getCarbonHome().toString(),
-                                                                             EventSimulatorConstants.DIRECTORY_DEPLOYMENT, EventSimulatorConstants.DIRECTORY_CSV_FILES).toString());
-        log.info("Event Simulator file service component is activated");
+            EventSimulatorConstants.DIRECTORY_DEPLOYMENT, EventSimulatorConstants.DIRECTORY_CSV_FILES).toString());
+        if (log.isDebugEnabled()) {
+            log.debug("Event Simulator file service component is activated");
+        }
     }
 
     /**
