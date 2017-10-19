c042ff28ed5bc3c86f1806cbb5ad1e3065222c19
piyumi_rameshka <rameshkafox@gmail.com>
2017-10-19 20:00:31 +0530

    formatting test class

--- components/org.wso2.carbon.das.jobmanager.core/src/test/java/org/wso2/carbon/das/jobmanager/core/SiddhiTopologyCreatorTestCase.java
+++ components/org.wso2.carbon.das.jobmanager.core/src/test/java/org/wso2/carbon/das/jobmanager/core/SiddhiTopologyCreatorTestCase.java
@@ -5,22 +5,28 @@ import org.wso2.carbon.das.jobmanager.core.topology.SiddhiTopologyCreatorImpl;
 
 public class SiddhiTopologyCreatorTestCase {
     @Test
-    public void testSiddhiTopologyCreator(){
+    public void testSiddhiTopologyCreator() {
 
-        String siddhiApp ="@App:name('TestPlan') \n" +
+        String siddhiApp = "@App:name('TestPlan') \n" +
                 "@source(type='http', receiver.url='http://localhost:9055/endpoints/stockQuote', @map(type='xml')) " +
                 "Define stream stockStream(symbol string, price float, quantity int, tier string);\n" +
                 "\n" +
-                "@Sink(type='email', @map(type='json'), username='wso2', address='test@wso2.com',password='****',host='smtp.gmail.com',subject='Event from SP',to='towso2@gmail.com')\n" +
+                "@Sink(type='email', @map(type='json'), username='wso2', address='test@wso2.com',password='****',"
+                + "host='smtp.gmail.com',subject='Event from SP',to='towso2@gmail.com')\n"
+                +
                 "Define stream takingOverStream(symbol string, overtakingSymbol string, avgPrice double);\n" +
                 "\n" +
                 "@source(type='http', receiver.url='http://localhost:9055/endpoints/trigger', @map(type='xml'))\n" +
                 "Define stream companyTriggerStream(symbol string);\n" +
                 "\n" +
-                "@Store(type='rdbms', jdbc.url='jdbc:mysql://localhost:3306/cepDB',jdbc.driver.name='', username='root', password='****',field.length='symbol:254')\n" +
+                "@Store(type='rdbms', jdbc.url='jdbc:mysql://localhost:3306/cepDB',jdbc.driver.name='', "
+                + "username='root', password='****',field.length='symbol:254')\n"
+                +
                 "Define table filteredTable (symbol string, price float, quantity int, tier string);\n" +
 
-                "@Store(type='rdbms', jdbc.url='jdbc:mysql://localhost:3306/spDB',jdbc.driver.name='', username='root', password='****',field.length='symbol:254')\n" +
+                "@Store(type='rdbms', jdbc.url='jdbc:mysql://localhost:3306/spDB',jdbc.driver.name='', "
+                + "username='root', password='****',field.length='symbol:254')\n"
+                +
                 "Define table takingOverTable(symbol string, overtakingSymbol string, avgPrice double);\n" +
                 "\n" +
                 "@info(name = 'query1')@dist(parallel='2', execGroup='001')\n" +
@@ -35,7 +41,8 @@ public class SiddhiTopologyCreatorTestCase {
                 "Select symbol, avg(price) as avgPrice, quantity\n" +
                 "Insert into #avgPriceStream;\n" +
                 "\n" +
-                "From #avgPriceStream#window.time(5 min) as a right outer join companyTriggerStream#window.length(1)\n" +
+                "From #avgPriceStream#window.time(5 min) as a right outer join companyTriggerStream#window.length(1)\n"
+                +
                 "On (companyTriggerStream.symbol == a.symbol)\n" +
                 "Select a.symbol, a.avgPrice, a.quantity\n" +
                 "Insert into triggeredAvgStream;\n" +
