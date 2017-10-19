a9d82ebb9610836536fbdcb2e227be0faf2f2cce
minudika <minudika001@gmail.com>
2017-10-19 19:22:03 +0530

    Implement creating table if not exists. Refactoring.

--- components/org.wso2.carbon.business.rules.core/src/main/java/org/wso2/carbon/business/rules/core/datasource/DatasourceConstants.java
+++ components/org.wso2.carbon.business.rules.core/src/main/java/org/wso2/carbon/business/rules/core/datasource/DatasourceConstants.java
@@ -21,11 +21,11 @@ package org.wso2.carbon.business.rules.core.datasource;
  * **/
 class DatasourceConstants {
     static final String componentNamespace = "business.rules";
-    static final String DATASOURCE_NAME = "business_rules_db";
 
     static final String ADD_BUSINESS_RULE = "ADD_BUSINESS_RULE";
     static final String UPDATE_BUSINESS_RULE = "UPDATE_BUSINESS_RULE";
     static final String UPDATE_DEPLOYMENT_STATUS = "UPDATE_DEPLOYMENT_STATUS";
+    static final String CREATE_TABLE = "CREATE_TABLE";
 
     static final String RETRIEVE_BUSINESS_RULE = "RETRIEVE_BUSINESS_RULE";
     static final String DELETE_BUSINESS_RULE = "DELETE_BUSINESS_RULE";
