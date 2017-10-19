f82610730a4df08c50ef5d5423a7f694662795ee
tishan89 <dtishanpubudu@gmail.com>
2017-10-19 20:05:58 +0530

    addingConstants

--- components/org.wso2.carbon.das.jobmanager.core/src/main/java/org/wso2/carbon/das/jobmanager/core/util/DistributedConstants.java
+++ components/org.wso2.carbon.das.jobmanager.core/src/main/java/org/wso2/carbon/das/jobmanager/core/util/DistributedConstants.java
@@ -14,4 +14,31 @@ public class DistributedConstants {
 
     public static final String MODE_HA = "ha";
 
+    //App creator constants
+    public static final String APP_NAME = "appName";
+    public static final String TOPIC_LIST = "topicList";
+    public static final String CONSUMER_GROUP_ID = "groupID";
+    public static final String BOOTSTRAP_SERVER_URL = "bootstrapServerURL";
+    public static final String PARTITION_LIST = "partitionList";
+    public static final String PARTITION_KEY = "partitionKey";
+    public static final String DESTINATIONS = "destinations";
+    public static final String PARTITION_NO = "partitionNo";
+
+    public static final String DEFAULT_KAFKA_SOURCE_TEMPLATE = "@source(type='kafka', topic.list='${" + TOPIC_LIST +
+            "}', group.id='${" + CONSUMER_GROUP_ID + "}', threading.option='partition.wise', bootstrap.servers='${"
+            + BOOTSTRAP_SERVER_URL + "}', @map(type='binary'))";
+    public static final String PARTITIONED_KAFKA_SOURCE_TEMPLATE =
+            "@source(type='kafka', topic.list='${" + TOPIC_LIST + "}', group.id='${" + CONSUMER_GROUP_ID + "}', "
+                    + "threading.option='partition.wise', bootstrap.servers='${" + BOOTSTRAP_SERVER_URL + "}', "
+                    + "partition.no.list='${" + PARTITION_LIST + "}',@map(type='binary'))";
+
+    public static final String DEFAULT_KAFKA_SINK_TEMPLATE = "@sink(type='kafka', topic='${" + TOPIC_LIST +
+            "}' , bootstrap.servers='${" + BOOTSTRAP_SERVER_URL + "}', @map(type='binary'))";
+
+    public static final String PARTITIONED_KAFKA_SINK_TEMPLATE = "@sink(type='kafka', topic='${" + TOPIC_LIST +
+            "}' , bootstrap.servers='${" + BOOTSTRAP_SERVER_URL + "}', @map(type='binary'), @distribution"
+            + "(strategy='partitioned', partitionKey='${" + PARTITION_KEY + "}', ${" + DESTINATIONS + "} )";
+
+    public static final String DESTINATION = "@destination(partition.no = ${" + PARTITION_NO + "})";
+
 }
