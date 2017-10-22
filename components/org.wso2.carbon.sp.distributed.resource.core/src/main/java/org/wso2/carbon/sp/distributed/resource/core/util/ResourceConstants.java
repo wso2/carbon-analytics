7e1cb7360f8c5aa4d6093ca2ffc9be0dcd676931
Grainier Perera <grainier@wso2.com>
2017-10-22 12:18:56 +0530

    Initial impl of the "Resource" component

---
+++ components/org.wso2.carbon.sp.distributed.resource.core/src/main/java/org/wso2/carbon/sp/distributed/resource/core/util/ResourceConstants.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.sp.distributed.resource.core.util;
+
+public class ResourceConstants {
+    public static final String RUNTIME_NAME_WORKER = "default"; // TODO: 10/15/17 For testing, change to worker
+    public static final String DEPLOYMENT_CONFIG_NS = "deployment.config";
+    public static final String MODE_DISTRIBUTED = "distributed";
+    public static final String RES_JOINED_STATE_NEW = "NEW";
+    public static final String RES_JOINED_STATE_EXISTS = "EXISTS";
+}
