b37899e3f55b35bde1608c756c5c30372971f359
dnwick <damithn@wso2.com>
2017-11-02 17:01:50 +0530

    removed unnessary change

--- components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/commons/js/dialog/save-to-file-dialog.js
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/commons/js/dialog/save-to-file-dialog.js
@@ -307,7 +307,6 @@ define(['require', 'lodash', 'jquery', 'log', 'backbone', 'file_browser', 'boots
                                 app.workspaceManager.updateMenuItems();
                                 log.debug('file saved successfully');
                                 siddhiFileEditor.setContent(config);
-                                app.
                                 callback(true);
                             } else {
                                 saveWizardError.text(data.Error);
