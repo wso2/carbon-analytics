2fb6eae83a5108ba4530dc0a325c5dadf4a11322
piyumi_rameshka <rameshkafox@gmail.com>
2017-11-22 16:25:15 +0530

    Storing constants

---
+++ components/org.wso2.carbon.das.yarn.core/src/main/java/org/wso2/carbon/das/yarn/core/utils/YarnDeploymentConstants.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.das.yarn.core.utils;
+
+public class YarnDeploymentConstants {
+
+    public static final String MODE_DISTRIBUTED ="yarn";
+    public static final String DEPLOYMENT_CONFIG_NS = "deployment.config";
+}
