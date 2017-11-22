4beb5b45c4f2f5021b4fce6bcb2299ab01505f30
piyumi_rameshka <rameshkafox@gmail.com>
2017-11-22 16:24:05 +0530

    Distribution service Yarn

---
+++ components/org.wso2.carbon.das.yarn.core/src/main/java/org/wso2/carbon/das/yarn/core/impl/YarnDistributionServiceImpl.java
@@ -0,0 +1,53 @@
+package org.wso2.carbon.das.yarn.core.impl;
+
+import org.wso2.carbon.das.jobmanager.core.DeploymentManager;
+import org.wso2.carbon.das.jobmanager.core.SiddhiAppCreator;
+import org.wso2.carbon.das.jobmanager.core.SiddhiTopologyCreator;
+import org.wso2.carbon.das.jobmanager.core.appCreator.DeployableSiddhiQueryGroup;
+import org.wso2.carbon.das.jobmanager.core.appCreator.DistributedSiddhiQuery;
+import org.wso2.carbon.das.jobmanager.core.topology.SiddhiTopology;
+import org.wso2.carbon.das.jobmanager.core.topology.SiddhiTopologyCreatorImpl;
+import org.wso2.carbon.das.yarn.core.internal.ServiceDataHolder;
+import org.wso2.carbon.stream.processor.core.distribution.DeploymentStatus;
+import org.wso2.carbon.stream.processor.core.distribution.DistributionService;
+import org.wso2.carbon.stream.processor.core.util.DeploymentMode;
+import org.wso2.carbon.stream.processor.core.util.RuntimeMode;
+
+import java.util.List;
+
+public class YarnDistributionServiceImpl implements DistributionService {
+    private SiddhiAppCreator appCreator;
+    private DeploymentManager deploymentManager;
+    private SiddhiTopologyCreator siddhiTopologyCreator;
+
+    public YarnDistributionServiceImpl(SiddhiAppCreator appCreator, DeploymentManager deploymentManager) {
+        this.appCreator = appCreator;
+        this.deploymentManager = deploymentManager;
+        siddhiTopologyCreator = new SiddhiTopologyCreatorImpl();
+    }
+
+    @Override public DeploymentStatus distribute(String userDefinedSiddhiApp) {
+        SiddhiTopology topology = siddhiTopologyCreator.createTopology(userDefinedSiddhiApp);
+        List<DeployableSiddhiQueryGroup> deployableQueryGroup = appCreator.createApps(topology);
+        return deploymentManager.deploy(new DistributedSiddhiQuery(topology.getName(), deployableQueryGroup));
+    }
+
+    @Override public RuntimeMode getRuntimeMode() {
+        return RuntimeMode.MANAGER;    }
+
+    @Override public DeploymentMode getDeploymentMode() {
+        return ServiceDataHolder.getDeploymentMode();
+    }
+
+    @Override public boolean isDistributed(String parentSiddhiAppName) {
+        return deploymentManager.isDeployed(parentSiddhiAppName);
+    }
+
+    @Override public void undeploy(String parentSiddhiAppName) {
+        deploymentManager.unDeploy(parentSiddhiAppName);
+    }
+
+    @Override public boolean isLeader() {
+        return false;
+    }
+}
