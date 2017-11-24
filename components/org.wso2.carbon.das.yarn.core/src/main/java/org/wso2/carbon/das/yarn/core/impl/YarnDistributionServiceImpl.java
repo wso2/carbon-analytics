e23bd5d712c4e8d1c9068b3ce91925c3c3db1f64
piyumi_rameshka <rameshkafox@gmail.com>
2017-11-24 08:56:14 +0530

    testing for yarn

--- components/org.wso2.carbon.das.yarn.core/src/main/java/org/wso2/carbon/das/yarn/core/impl/YarnDistributionServiceImpl.java
+++ components/org.wso2.carbon.das.yarn.core/src/main/java/org/wso2/carbon/das/yarn/core/impl/YarnDistributionServiceImpl.java
@@ -1,13 +1,14 @@
 package org.wso2.carbon.das.yarn.core.impl;
 
+import org.apache.log4j.Logger;
 import org.wso2.carbon.das.jobmanager.core.DeploymentManager;
 import org.wso2.carbon.das.jobmanager.core.SiddhiAppCreator;
 import org.wso2.carbon.das.jobmanager.core.SiddhiTopologyCreator;
 import org.wso2.carbon.das.jobmanager.core.appCreator.DeployableSiddhiQueryGroup;
 import org.wso2.carbon.das.jobmanager.core.appCreator.DistributedSiddhiQuery;
+import org.wso2.carbon.das.jobmanager.core.internal.ServiceDataHolder;
 import org.wso2.carbon.das.jobmanager.core.topology.SiddhiTopology;
 import org.wso2.carbon.das.jobmanager.core.topology.SiddhiTopologyCreatorImpl;
-import org.wso2.carbon.das.yarn.core.internal.ServiceDataHolder;
 import org.wso2.carbon.stream.processor.core.distribution.DeploymentStatus;
 import org.wso2.carbon.stream.processor.core.distribution.DistributionService;
 import org.wso2.carbon.stream.processor.core.util.DeploymentMode;
@@ -19,8 +20,10 @@ public class YarnDistributionServiceImpl implements DistributionService {
     private SiddhiAppCreator appCreator;
     private DeploymentManager deploymentManager;
     private SiddhiTopologyCreator siddhiTopologyCreator;
+    private final Logger LOG = Logger.getLogger(YarnDistributionServiceImpl.class);
 
     public YarnDistributionServiceImpl(SiddhiAppCreator appCreator, DeploymentManager deploymentManager) {
+        LOG.info("************************initializing "+ this.getClass().getName());
         this.appCreator = appCreator;
         this.deploymentManager = deploymentManager;
         siddhiTopologyCreator = new SiddhiTopologyCreatorImpl();
