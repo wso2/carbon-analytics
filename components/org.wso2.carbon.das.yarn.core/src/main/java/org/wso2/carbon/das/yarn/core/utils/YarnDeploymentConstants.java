08e78bd213b1e98eb5e39c35adb88a3f2d925b1a
piyumi_rameshka <rameshkafox@gmail.com>
2017-11-29 15:56:26 +0530

    updating constants

--- components/org.wso2.carbon.das.yarn.core/src/main/java/org/wso2/carbon/das/yarn/core/utils/YarnDeploymentConstants.java
+++ components/org.wso2.carbon.das.yarn.core/src/main/java/org/wso2/carbon/das/yarn/core/utils/YarnDeploymentConstants.java
@@ -5,12 +5,11 @@ public class YarnDeploymentConstants {
     public static final String MODE_DISTRIBUTED ="yarn";
     public static final String DEPLOYMENT_CONFIG_NS = "deployment.config";
     public static final Integer SPAPP_MASTER_MEMORY = 256;
-    public static final Integer SPAPP_MASTER_VCORES =2;
+    public static final Integer SPAPP_MASTER_VCORES = 2;
     public static final String SP_APP_MASTER_QUEUE = "default";
-    public static final Integer SP_APP_MASTER_PRIORITY =0;
-    public static final String SPAPP_MASTER_JAR_PATH = "/home/piyumi/Documents/org.wso2.carbon.das.yarnapp"
-            + ".core/target/org.wso2.carbon.das.yarnapp.core-1.0-SNAPSHOT.jar";
+    public static final Integer SP_APP_MASTER_PRIORITY = 0;
     public static final String SPAPP_MASTER_MAIN_CLASS ="org.wso2.carbon.das.yarnapp.core.SPAPPMaster";
     public static final String SIDDHIAPP_HOLDER_HDFS_PATH ="siddhiappholderList.ser";
     public static final String SPAPP_MASTER = "SPAPPMaster.jar";
+    public static final String YARN_DEPLOYMENT_CONFIG ="yarn.config";
 }
