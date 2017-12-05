e1bf40b1529b20437b3cb7ba907e9494d18ca156
ramindu90 <ramindus@wso2.com>
2017-12-05 19:16:12 +0530

    updating license header

--- components/org.wso2.carbon.event.simulator.core/src/main/java/org/wso2/carbon/event/simulator/core/impl/FeedApiServiceImpl.java
+++ components/org.wso2.carbon.event.simulator.core/src/main/java/org/wso2/carbon/event/simulator/core/impl/FeedApiServiceImpl.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.carbon.event.simulator.core.impl;
 
-import com.google.gson.JsonObject;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.wso2.carbon.event.simulator.core.api.FeedApiService;
@@ -24,7 +41,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaMSF4JServerCodegen",
