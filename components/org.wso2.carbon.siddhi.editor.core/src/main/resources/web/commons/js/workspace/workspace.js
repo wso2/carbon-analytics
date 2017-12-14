43dc8d55eb8ce7cfd0901847428596c90b3d8cc4
Damith Wickramasinghe <dnwick@Damiths-MacBook-Pro.local>
2017-12-15 02:28:52 +0530

    added fixes

--- components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/commons/js/workspace/workspace.js
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/commons/js/workspace/workspace.js
@@ -350,11 +350,7 @@ define(['ace/ace', 'jquery', 'lodash', 'log','dialogs','./service-client','welco
 
                 if(file !== undefined){
                     file = activeTab.getFile();
-                    var runningStatus = false;
-                    if(file.getRunStatus() || file.getDebugStatus()){
-                        runningStatus = true;
-                    }
-                    if(file.isDirty() && !runningStatus){
+                    if(file.isDirty()){
                         runMenuItem.disable();
                         debugMenuItem.disable();
                         stopMenuItem.disable();
@@ -362,7 +358,7 @@ define(['ace/ace', 'jquery', 'lodash', 'log','dialogs','./service-client','welco
                         toolBar.disableDebugButton();
                         toolBar.disableStopButton();
                     } else {
-                        if(runningStatus){
+                        if(file.getRunStatus() || file.getDebugStatus()){
                             runMenuItem.disable();
                             debugMenuItem.disable();
                             stopMenuItem.enable();
