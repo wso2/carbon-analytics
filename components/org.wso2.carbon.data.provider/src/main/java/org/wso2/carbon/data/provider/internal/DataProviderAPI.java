5ea213028ee3ffc1e14f4561054348af6fa41d73
Sajith Janaprasad <sajithar@wso2.com>
2018-04-27 11:10:56 +0530

    Add DashboardRestApiProvider class.

--- components/org.wso2.carbon.data.provider/src/main/java/org/wso2/carbon/data/provider/internal/DataProviderAPI.java
+++ components/org.wso2.carbon.data.provider/src/main/java/org/wso2/carbon/data/provider/internal/DataProviderAPI.java
@@ -19,13 +19,7 @@
 package org.wso2.carbon.data.provider.internal;
 
 import com.google.gson.JsonElement;
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.carbon.analytics.msf4j.interceptor.common.AuthenticationInterceptor;
@@ -48,57 +42,27 @@ import static org.wso2.carbon.data.provider.utils.DataProviderValueHolder.getDat
 /**
  * Data provider api service component.
  */
-@Component(
-        name = "data-provider-api",
-        service = Microservice.class,
-        immediate = true
-)
-@Path("/portal/apis/data-provider")
 @RequestInterceptor(AuthenticationInterceptor.class)
 public class DataProviderAPI implements Microservice {
+
+    public static final String API_CONTEXT_PATH = "/apis/data-provider";
     private static final Logger log = LoggerFactory.getLogger(DataProviderAPI.class);
-
-    public DataProviderAPI() {
-    }
-
-    /**
-     * This is the activation method of ServiceComponent. This will be called when its references are
-     * satisfied.
-     *
-     * @param bundleContext the bundle context instance of this bundle.
-     * @throws Exception this will be thrown if an issue occurs while executing the activate method
-     */
-    @Activate
-    protected void start(BundleContext bundleContext) throws Exception {
-        log.info("Data Provider Service Component is activated");
-    }
-
-    /**
-     * This is the deactivation method of ServiceComponent. This will be called when this component
-     * is being stopped or references are satisfied during runtime.
-     *
-     * @throws Exception this will be thrown if an issue occurs while executing the de-activate method
-     */
-    @Deactivate
-    protected void stop() throws Exception {
-        log.info("Data Provider Service Component is deactivated");
-    }
 
     @GET
     @Path("/list")
     @Produces("application/json")
     public Response dataProviderList() {
         try {
-            return Response.status(Response.Status.OK)
+            return Response.ok()
                     .entity(getDataProviderHelper().getDataProviderNameSet())
                     .type(MediaType.APPLICATION_JSON)
                     .build();
-        } catch (Throwable throwable) {
-            log.error("Server error occurred when listing data providers.", throwable);
-            return Response.serverError().entity("Server error occurred when listing data providers: " +
-                    throwable.getMessage()).build();
+        } catch (Exception e) {
+            log.error("Cannot list data providers.", e);
+            return Response.serverError()
+                    .entity("Server error occurred when listing data providers.")
+                    .build();
         }
-
     }
 
     @GET
@@ -106,15 +70,16 @@ public class DataProviderAPI implements Microservice {
     @Produces("application/json")
     public Response dataProviderConfig(@PathParam("providerName") String providerName) {
         try {
-            return Response.status(Response.Status.OK)
+            return Response.ok()
                     .entity(getDataProviderHelper().getDataProvider(providerName).providerConfig())
                     .type(MediaType.APPLICATION_JSON)
                     .build();
-        } catch (Throwable throwable) {
-            log.error("Server error occurred when retrieving configuration of data provider: '{}'.",
-                    replaceCRLFCharacters(providerName), throwable);
-            return Response.serverError().entity("Server error occurred when when retrieving configuration " +
-                    "of data provider: '" + providerName + "'. " + throwable.getMessage()).build();
+        } catch (IllegalAccessException | InstantiationException e) {
+            log.error("Cannot retrieve configuration of data provider '{}'.", replaceCRLFCharacters(providerName), e);
+            return Response.serverError()
+                    .entity("Server error occurred when when retrieving configuration of data provider '" +
+                            providerName + "'. ")
+                    .build();
         }
 
     }
@@ -129,29 +94,18 @@ public class DataProviderAPI implements Microservice {
             DataProvider dataProvider = getDataProviderHelper().getDataProvider(providerName);
             //mock the topic and session as it only needs to validate the configuration.
             dataProvider.init("mock_topic", "mock_session", dataProviderConfig);
-            return Response.ok(dataProvider.dataSetMetadata(), MediaType.APPLICATION_JSON)
+            return Response.ok()
+                    .entity(dataProvider.dataSetMetadata())
+                    .type(MediaType.APPLICATION_JSON)
                     .build();
         } catch (DataProviderException | IllegalAccessException | InstantiationException e) {
-            log.error("Server error occurred when validating the provider name: '{}'.",
-                    replaceCRLFCharacters(providerName), e);
-            return Response.serverError().entity("Server error occurred when validating the provider name: '" +
-                    providerName + "'. " + e.getMessage()).build();
+            log.error("Cannot validate configurations '{}' for data provider '{}'.", dataProviderConfig,
+                      replaceCRLFCharacters(providerName), e);
+            return Response.serverError()
+                    .entity("Server error occurred when validating configuration for data provider '" + providerName +
+                            "'.")
+                    .build();
         }
-    }
-
-    @Reference(
-            name = "data-providers-service",
-            service = DataProvider.class,
-            cardinality = ReferenceCardinality.AT_LEAST_ONE,
-            policy = ReferencePolicy.DYNAMIC,
-            unbind = "unsetCarbonTransport"
-    )
-    protected void setCarbonTransport(DataProvider dataProvider) {
-        getDataProviderHelper().setDataProvider(dataProvider.providerName(), dataProvider);
-    }
-
-    protected void unsetCarbonTransport(DataProvider dataProvider) {
-        getDataProviderHelper().removeDataProviderClass(dataProvider.providerName());
     }
 
     private String replaceCRLFCharacters(String str) {
