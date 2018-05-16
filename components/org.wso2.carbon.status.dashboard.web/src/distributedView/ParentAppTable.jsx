900b1ab8758938ab55640484a8e09b16b4ecdc4d
GowthamyVaseekaran <vaseekarangowthamy@gmail.com>
2018-05-16 22:16:14 +0530

    Refactor implementation

--- components/org.wso2.carbon.status.dashboard.web/src/distributedView/ParentAppTable.jsx
+++ components/org.wso2.carbon.status.dashboard.web/src/distributedView/ParentAppTable.jsx
@@ -168,7 +168,6 @@ export default class ParentAppTable extends React.Component {
                         onChange={number => {
                             this.currentPage = number;
                             this.loadData(number, this.state.appsList);
-                            console.log("applist"+this.state.appsList)
                         }}
                     />
                 </div>
