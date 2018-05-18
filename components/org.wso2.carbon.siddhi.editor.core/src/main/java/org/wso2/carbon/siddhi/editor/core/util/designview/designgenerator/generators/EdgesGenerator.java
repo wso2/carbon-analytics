4c22169d291259b25e35be148119edf4e135bdec
senthuran16 <senthuranrc16@gmail.com>
2018-05-18 11:43:52 +0530

    Clean EdgesGenerator class

--- components/org.wso2.carbon.siddhi.editor.core/src/main/java/org/wso2/carbon/siddhi/editor/core/util/designview/designgenerator/generators/EdgesGenerator.java
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/java/org/wso2/carbon/siddhi/editor/core/util/designview/designgenerator/generators/EdgesGenerator.java
@@ -48,23 +48,34 @@ public class EdgesGenerator {
      */
     public List<Edge> generateEdges() {
         List<Edge> edges = new ArrayList<>();
-        for (QueryConfig windowFilterProjectionQuery : siddhiAppConfig.getWindowFilterProjectionQueryList()) {
-            // Edge for Input
-            edges.add(generateEdge(
-                    ((WindowFilterProjectionConfig) (windowFilterProjectionQuery.getQueryInput())).getFrom(),
-                    windowFilterProjectionQuery.getId()));
-            // Edge for Output
-            edges.add(generateEdge(
-                    windowFilterProjectionQuery.getId(),
-                    windowFilterProjectionQuery.getQueryOutput().getTarget()));
+        for (QueryConfig query : siddhiAppConfig.getWindowFilterProjectionQueryList()) {
+            // Edge towards Query element
+            edges.add(
+                    generateEdge(
+                            getElementWithStreamName(
+                                    ((WindowFilterProjectionConfig) (query.getQueryInput())).getFrom()).getId(),
+                            query.getId()));
+            // Edge from Query element
+            edges.add(
+                    generateEdge(
+                            query.getId(),
+                            getElementWithStreamName(query.getQueryOutput().getTarget()).getId()));
         }
-        for (QueryConfig joinQuery : siddhiAppConfig.getJoinQueryList()) {
-            // Edge for Input (Left)
-            edges.add(generateEdge(((JoinConfig)(joinQuery.getQueryInput())).getLeft().getFrom(), joinQuery.getId()));
-            // Edge for Input (Right)
-            edges.add(generateEdge(((JoinConfig)(joinQuery.getQueryInput())).getRight().getFrom(), joinQuery.getId()));
-            // Edge for Output
-            edges.add(generateEdge(joinQuery.getId(), joinQuery.getQueryOutput().getTarget()));
+        for (QueryConfig query : siddhiAppConfig.getJoinQueryList()) {
+            // Edge towards Query element (From Left element)
+            edges.add(
+                    generateEdge(
+                            getElementWithStreamName(
+                                    ((JoinConfig) (query.getQueryInput())).getLeft().getFrom()).getId(),
+                            query.getId()));
+            // Edge towards Query element (From Right element)
+            edges.add(
+                    generateEdge(
+                            getElementWithStreamName(
+                                    ((JoinConfig) (query.getQueryInput())).getRight().getFrom()).getId(),
+                            query.getId()));
+            // Edge from Query element
+            edges.add(generateEdge(query.getId(), query.getQueryOutput().getTarget()));
         }
         // TODO: 3/29/18 implement other edges
         return edges;
@@ -72,23 +83,57 @@ public class EdgesGenerator {
 
     /**
      * Returns an Edge, that represents the connection between given parent and child, denoted by their Ids
-     * @param parentId      Id of the Parent Node
-     * @param childId       Id of the Child Node
-     * @return              Edge which connects the given Parent and Child
+     * @param parentId      Id of the edge's starting Node
+     * @param childId       Id of the edge's ending Node
+     * @return              Edge which represents the connection between parent and child elements
      */
     private Edge generateEdge(String parentId, String childId) {
-        NodeType parentType = getSiddhiElementType(getElementById(parentId));
-        NodeType childType = getSiddhiElementType(getElementById(childId));
+        NodeType parentType = getSiddhiElementType(getElementWithId(parentId));
+        NodeType childType = getSiddhiElementType(getElementWithId(childId));
         String edgeId = DesignGeneratorHelper.generateEdgeID(parentId, childId);
         return new Edge(edgeId, parentId, parentType, childId, childType);
     }
 
     /**
-     * Gets SiddhiElementConfig object from the SiddhiAppConfig, that has the given Id
-     * @param id        Id of the Siddhi Element
+     * Gets SiddhiElementConfig object from the SiddhiAppConfig, which has a related stream with the given name
+     * @param streamName        Name of the SiddhiElementConfig's related stream
+     * @return                  SiddhiElementConfig object
+     */
+    private SiddhiElementConfig getElementWithStreamName(String streamName) {
+        for (StreamConfig streamConfig : siddhiAppConfig.getStreamList()) {
+            if (streamConfig.getName().equals(streamName)) {
+                return streamConfig;
+            }
+        }
+        for (TableConfig tableConfig : siddhiAppConfig.getTableList()) {
+            if (tableConfig.getName().equals(streamName)) {
+                return tableConfig;
+            }
+        }
+        for (TriggerConfig triggerConfig : siddhiAppConfig.getTriggerList()) {
+            if (triggerConfig.getName().equals(streamName)) {
+                return triggerConfig;
+            }
+        }
+        for (WindowConfig windowConfig : siddhiAppConfig.getWindowList()) {
+            if (windowConfig.getName().equals(streamName)) {
+                return windowConfig;
+            }
+        }
+        for (AggregationConfig aggregationConfig : siddhiAppConfig.getAggregationList()) {
+            if (aggregationConfig.getName().equals(streamName)) {
+                return aggregationConfig;
+            }
+        }
+        throw new IllegalArgumentException("Unable to find an element with related stream name '" + streamName + "'");
+    }
+
+    /**
+     * Gets SiddhiElementConfig object from the SiddhiAppConfig, which has the given Id
+     * @param id        Id of the SiddhiElementConfig
      * @return          SiddhiElementConfig object
      */
-    private SiddhiElementConfig getElementById(String id) {
+    private SiddhiElementConfig getElementWithId(String id) {
         for (SiddhiElementConfig siddhiElementConfig : siddhiAppConfig.getWindowFilterProjectionQueryList()) {
             if (siddhiElementConfig.getId().equals(id)) {
                 return siddhiElementConfig;
@@ -144,7 +189,7 @@ public class EdgesGenerator {
                 return siddhiElementConfig;
             }
         }
-        throw new IllegalArgumentException("Unable to find an element with the id '" + id + "'");
+        throw new IllegalArgumentException("Unable to find element with id '" + id + "'");
     }
 
     /**
@@ -179,6 +224,7 @@ public class EdgesGenerator {
             throw new IllegalArgumentException("Type is unknown for Query Input");
         }
         // TODO implement
-        throw new IllegalArgumentException("Type is unknown for Siddhi Element '" + siddhiElementConfig.getId() + "'");
+        throw new IllegalArgumentException(
+                "Type is unknown for Siddhi Element with id '" + siddhiElementConfig.getId() + "'");
     }
 }
