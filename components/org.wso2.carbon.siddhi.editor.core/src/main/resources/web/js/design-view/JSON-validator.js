387b7155b16a8705fa7879fde498e8d0ffba50be
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2018-06-18 17:34:53 +0530

    Add initial implementation for json validator class

---
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/js/design-view/JSON-validator.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+define(['require', 'log', 'lodash'],
+    function (require, log, _) {
+
+        /**
+         * @class JSONValidator
+         * @constructor
+         * @class JSONValidator  Validates a given JSON structure by looking for minimum required fields for particular
+         * element
+         */
+        var JSONValidator = function (JSON) {
+        };
+
+        /**
+         * @function Validates a given JSON
+         * @param JSON provided JSON
+         */
+        JSONValidator.prototype.validate = function (JSON) {
+            
+        };
+
+        function validateWindowFilterProjectionQuery() {
+
+        }
+
+        function validateJoinQuery() {
+
+        }
+
+        function validatePatternQuery() {
+
+        }
+
+        function validateSequenceQuery() {
+
+        }
+
+        function validateQuerySelectSection() {
+
+        }
+
+        function validateQueryOutputSection() {
+
+        }
+
+        function validateJoinQuerySource() {
+
+        }
+
+        return JSONValidator;
+    });
