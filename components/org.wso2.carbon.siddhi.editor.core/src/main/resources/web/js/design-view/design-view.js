adb71cc2bf6740a71faa6a56029b40ff3d26a7bd
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2018-06-19 18:06:22 +0530

    Allow users to switch to design view when app is empty

--- components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/js/design-view/design-view.js
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/js/design-view/design-view.js
@@ -147,32 +147,35 @@ define(['require', 'log', 'lodash', 'jquery', 'tool_palette/tool-palette', 'desi
 
             // check whether Siddhi app is in initial mode(initial Siddhi app template) and if yes then go to the
             // design view with the no content
+            var defaultResponse = {
+                "siddhiAppConfig": {
+                    "siddhiAppName": match[1],
+                        "appAnnotationList": ['@App:description("' + match[2] + '")'],
+                        "streamList": [],
+                        "tableList": [],
+                        "windowList": [],
+                        "triggerList": [],
+                        "aggregationList": [],
+                        "functionList": [],
+                        "partitionList": [],
+                        "sourceList": [],
+                        "sinkList": [],
+                        "queryLists": {
+                        "WINDOW_FILTER_PROJECTION": [],
+                            "PATTERN": [],
+                            "SEQUENCE": [],
+                            "JOIN": []
+                    },
+                    "finalElementCount": 0
+                },
+                "edgeList": []
+            };
+
+            var defaultString = JSON.stringify(defaultResponse);
             if (match !== null) {
                 result = {
                     status: "success",
-                    responseJSON: {
-                        "siddhiAppConfig": {
-                            "siddhiAppName": match[1],
-                            "appAnnotationList": ['@App:description("' + match[2] + '")'],
-                            "streamList": [],
-                            "tableList": [],
-                            "windowList": [],
-                            "triggerList": [],
-                            "aggregationList": [],
-                            "functionList": [],
-                            "partitionList": [],
-                            "sourceList": [],
-                            "sinkList": [],
-                            "queryLists": {
-                                "WINDOW_FILTER_PROJECTION": [],
-                                "PATTERN": [],
-                                "SEQUENCE": [],
-                                "JOIN": []
-                            },
-                            "finalElementCount": 0
-                        },
-                        "edgeList": []
-                    }
+                    responseString: defaultString
                 };
             } else {
                 self.codeToDesignURL = window.location.protocol + "//" + window.location.host + "/editor/design-view";
