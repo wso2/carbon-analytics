c078ef90ce8b780f27549f27a93e16ffd3642a68
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2018-06-24 02:35:33 +0530

    Include partitions in getDefinitionElementById method

--- components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/js/design-view/configuration-data/app-data.js
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/js/design-view/configuration-data/app-data.js
@@ -341,9 +341,11 @@ define(['require', 'elementUtils', 'lodash'],
          * @param elementId id of the definition element
          * @param includeQueryTypes if true search in the queries as well
          * @param includeSourceAndSink if true search in the sinks and sources as well
+         * @param includePartitions if true search in the partition list as well
          * @return requestedElement returns undefined if the requested element is not found
          */
-        AppData.prototype.getDefinitionElementById = function (elementId, includeQueryTypes, includeSourceAndSink) {
+        AppData.prototype.getDefinitionElementById = function (elementId, includeQueryTypes, includeSourceAndSink,
+                                                               includePartitions) {
             var self = this;
             var requestedElement;
             var streamList = self.streamList;
@@ -358,6 +360,7 @@ define(['require', 'elementUtils', 'lodash'],
             var joinQueryList = self.queryLists.JOIN;
             var sourceList = self.sourceList;
             var sinkList = self.sinkList;
+            var partitionList = self.partitionList;
 
             var lists = [streamList, tableList, windowList, aggregationList, functionList, triggerList];
 
@@ -370,6 +373,9 @@ define(['require', 'elementUtils', 'lodash'],
             if (includeSourceAndSink !== undefined && includeSourceAndSink) {
                 lists.push(sourceList);
                 lists.push(sinkList);
+            }
+            if (includePartitions !== undefined && includePartitions) {
+                lists.push(partitionList);
             }
 
             _.forEach(lists, function (list) {
@@ -400,6 +406,8 @@ define(['require', 'elementUtils', 'lodash'],
                             type = 'SOURCE';
                         } else if (list === sinkList) {
                             type = 'SINK';
+                        } else if (list === partitionList) {
+                            type = 'PARTITION';
                         }
                         requestedElement = {
                             type: type,
