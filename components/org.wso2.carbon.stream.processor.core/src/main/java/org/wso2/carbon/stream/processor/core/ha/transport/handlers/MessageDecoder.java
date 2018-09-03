91486195f41ee276d68d8c32798167afdbb3b510
dnwick <dnsnipe123@gmail.com>
2018-09-04 04:27:00 +0530

    Add tps calculation

--- components/org.wso2.carbon.stream.processor.core/src/main/java/org/wso2/carbon/stream/processor/core/ha/transport/handlers/MessageDecoder.java
+++ components/org.wso2.carbon.stream.processor.core/src/main/java/org/wso2/carbon/stream/processor/core/ha/transport/handlers/MessageDecoder.java
@@ -21,7 +21,13 @@ package org.wso2.carbon.stream.processor.core.ha.transport.handlers;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import org.apache.log4j.Logger;
+import org.wso2.carbon.stream.processor.core.ha.util.HAConstants;
+import org.wso2.carbon.stream.processor.core.util.BinaryMessageConverterUtil;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
@@ -30,7 +36,10 @@ import java.util.concurrent.BlockingQueue;
  */
 public class MessageDecoder extends ByteToMessageDecoder {
     private BlockingQueue<ByteBuf> byteBufferQueue;
-
+    private static long startTime;
+    private static long endTime;
+    static int count = 0;
+    static final Logger log = Logger.getLogger(MessageDecoder.class);
     public MessageDecoder(BlockingQueue<ByteBuf> byteBufferQueue){
         this.byteBufferQueue = byteBufferQueue;
     }
@@ -47,6 +56,36 @@ public class MessageDecoder extends ByteToMessageDecoder {
             return;
         }
         byteBufferQueue.offer(in);
-        in.markReaderIndex();
+        try {
+
+            int channelIdSize = in.readInt();
+            String channelId = BinaryMessageConverterUtil.getString(in, channelIdSize);
+
+            int dataLength = in.readInt();
+            byte[] bytes = new byte[dataLength];
+            in.readBytes(bytes);
+            //LOG.info("Message Received : " + new String(bytes));
+
+            if (channelId.equals(HAConstants.CHANNEL_ID_MESSAGE)) {
+                ByteBuffer messageBuffer = ByteBuffer.wrap(bytes);
+                count++;
+                if (count % 10000 == 0) {
+                    log.info("COUNT " + count);
+                    endTime = new Date().getTime();
+                    //LOG.info("TIME START    " + time + "    TIMES   " + timeE);
+                    log.info("Server TPS:   " + (((10000 * 1000) / (endTime - startTime))));
+                    startTime = new Date().getTime();
+                }
+
+            } else if (channelId.equals(HAConstants.CHANNEL_ID_CONTROL_MESSAGE)) {
+
+                log.info("Control Message Received : " + new String(bytes));
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } finally {
+                    in.markReaderIndex();
+        }
+
     }
 }
