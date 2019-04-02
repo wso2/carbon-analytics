e710533d4ef0cc2273a0fc54437f1f9b05d0fbe5
PradeepaJey1 <pradeepaj@wso2.com>
2019-04-02 14:08:28 +0530

    Validate partition expression
    If the partition expression is not chosen by the user it displays the error message beneath the drop down without showing it as an design-view alert.

--- components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/js/design-view/form-builder/forms/partition-form.js
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/js/design-view/form-builder/forms/partition-form.js
@@ -142,14 +142,20 @@ define(['require', 'log', 'jquery', 'lodash', 'partitionWith', 'jsonValidator', 
 
                         var partitionKeys = [];
                         $('#partition-by-content .partition-key').each(function () {
-                            var expression = $(this).find('.partition-by-expression-selection').val();
-                            var streamName = $(this).find('.partition-by-stream-name').val().trim();
-                            var partitionKey = {
-                                expression: expression,
-                                streamName: streamName
-                            };
-                            partitionKeys.push(partitionKey);
-
+                            var expression = $(this).find('.partition-by-expression-selection');
+                            if (!expression.val()) {
+                                $(this).find('.error-message').text("Expression is required");
+                                self.formUtils.addErrorClass(expression);
+                                isErrorOccurred = true;
+                                return false;
+                            } else {
+                                var streamName = $(this).find('.partition-by-stream-name').val().trim();
+                                var partitionKey = {
+                                    expression: expression.val(),
+                                    streamName: streamName
+                                };
+                                partitionKeys.push(partitionKey);
+                            }
                         });
 
                         if (!isErrorOccurred) {
