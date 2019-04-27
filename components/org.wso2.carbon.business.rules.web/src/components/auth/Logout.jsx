c25e134cad7602d98d63f84ff6c9b318e1a797cf
Niveathika <niveathika@wso2.com>
2019-04-27 19:58:52 +0530

    Add SSO Support for business rules

--- components/org.wso2.carbon.business.rules.web/src/components/auth/Logout.jsx
+++ components/org.wso2.carbon.business.rules.web/src/components/auth/Logout.jsx
@@ -21,6 +21,7 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 // Auth Utils
 import AuthManager from '../../utils/AuthManager';
+import { Constants } from "../../utils/Constants";
 
 /**
  * App context.
@@ -34,19 +35,27 @@ export default class Logout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirectToLogin: false,
+      redirectUrl :''
     };
   }
 
   componentDidMount() {
-    AuthManager.logout()
-      .then(() => this.setState({ redirectToLogin: true }));
+    AuthManager.getAuthType()
+        .then((response) => {
+          if (response.data.authType === Constants.AUTH_TYPE_SSO) {
+            AuthManager.ssoLogout().then((redirectUrl) => {
+              location.href = redirectUrl;
+            });
+          } else {
+            AuthManager.logout()
+                .then(() => {
+                  this.setState({ redirectUrl: '/login' });
+                });
+          }
+        });
   }
 
   render() {
-    if (this.state.redirectToLogin) {
-      return <Redirect to={{ pathname: '/login' }} />;
-    }
-    return null;
+    return <Redirect to={{ pathname: this.state.redirectUrl }} />;
   }
 }
