d5dd0d28a9cd301d39281d693c58f2d59d1ca8f8
Suhothayan <suhothayan@gmail.com>
2019-10-01 19:13:07 +0530

    Fix console reconnection issue in editor

--- src/main/resources/web/js/output-console/service-console-list-manager.js
+++ src/main/resources/web/js/output-console/service-console-list-manager.js
@@ -217,11 +217,32 @@ define(['log', 'jquery', 'lodash', 'output_console_list', 'workspace', 'service_
                         // ws.close();
                     };
                     ws.onclose = function (error) {
-                        console.log('Editor console service has terminated. Reconnect will be attempted in 1 second.',
-                            error.reason);
-                        setTimeout(function () {
-                            opts.application.outputController.initiateLogReader(opts);
-                        }, 1000);
+                        var console = opts.application.outputController.getGlobalConsole();
+                        if (console == undefined) {
+                            var consoleOptions = {};
+                            var options = {};
+                            _.set(options, '_type', "CONSOLE");
+                            _.set(options, 'title', "Console");
+                            _.set(options, 'statusForCurrentFocusedFile', "LOGGER");
+                            _.set(options, 'currentFocusedFile', undefined);
+                            _.set(consoleOptions, 'consoleOptions', options);
+                            console = opts.application.outputController.newConsole(consoleOptions);
+                            // opts.application.outputController.toggleOutputConsole();
+                            opts.application.outputController.hideAllConsoles();
+                        }
+                        var logMessage = "Connection closed, backend is unavailable! " +
+                            "Refresh to reconnect the console.";
+                        var message = {
+                            "type": "ERROR",
+                            "message": logMessage
+                        };
+                        console.println(message);
+                        // not retrying
+                        // console.log('Editor console service has terminated. Reconnect will be attempted in 1 second.',
+                        //     error.reason);
+                        // setTimeout(function () {
+                        //     opts.application.outputController.initiateLogReader(opts);
+                        // }, 1000);
                     };
                 }
 
