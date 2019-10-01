59729284758278ffcfa08c46ba09fef939b6083c
Chiran <pcnfernando@gmail.com>
2019-10-01 16:08:12 +0530

    Avoid syntax validation while app is running, validate when stopped

--- src/main/resources/web/js/launcher/launch-manager.js
+++ src/main/resources/web/js/launcher/launch-manager.js
@@ -107,6 +107,8 @@ define(['require', 'jquery', 'backbone', 'lodash', 'event_channel', 'console'],
                     success: function (data) {
                         if (console != undefined) {
                             activeTab.getFile().setStopProcessRunning(false);
+                            activeTab.getSiddhiFileEditor().getSourceView().getMainEditor()
+                                .onEnvironmentChange();
                             var msg = "";
                             activeTab.getFile().setRunStatus(false);
                             activeTab.getFile().save();
