ce77bdc07f066e51971be5439ccc7fb3b49e0323
Suhothayan <suhothayan@gmail.com>
2019-11-30 13:10:19 +0530

    add min files

--- src/main/resources/web/editor/commons/lib/mCustomScrollbar_v3.1.5/js/jquery.mCustomScrollbar.js
+++ src/main/resources/web/editor/commons/lib/mCustomScrollbar_v3.1.5/js/jquery.mCustomScrollbar.js
@@ -6,29 +6,9 @@
  Author URI: http://manos.malihu.gr
  License: MIT License (MIT)
  */
-
 /*
  Copyright Manos Malihutsakis (email: manos@malihu.gr)
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
-
+*/
 /*
  The code below is fairly long, fully commented and should be normally used in development.
  For production, use either the minified jquery.mCustomScrollbar.min.js script or
