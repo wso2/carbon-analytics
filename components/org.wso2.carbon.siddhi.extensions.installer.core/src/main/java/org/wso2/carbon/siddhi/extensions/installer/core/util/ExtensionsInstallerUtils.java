dababbe6e13144ec8f33ad5505eb4c05415d5f02
senthuran16 <senthuranrc16@gmail.com>
2020-02-11 17:08:15 +0530

    Re-indent ExtensionsInstallerUtils class

--- components/org.wso2.carbon.siddhi.extensions.installer.core/src/main/java/org/wso2/carbon/siddhi/extensions/installer/core/util/ExtensionsInstallerUtils.java
+++ components/org.wso2.carbon.siddhi.extensions.installer.core/src/main/java/org/wso2/carbon/siddhi/extensions/installer/core/util/ExtensionsInstallerUtils.java
@@ -45,126 +45,126 @@ import java.util.Map;
  * Contains utility methods that are used in Siddhi Extensions Installer.
  */
 public class ExtensionsInstallerUtils {
-	private ExtensionsInstallerUtils() {
-		// Prevents instantiation.
-	}
+    private ExtensionsInstallerUtils() {
+        // Prevents instantiation.
+    }
 
-	/**
-	 * Returns the extension which has the given id, from the given map of extension configurations.
-	 *
-	 * @param extensionId					Id of the extension.
-	 * @param extensionConfigs				Configurations of all available extensions.
-	 * @return								Configuration of the found extension.
-	 * @throws ExtensionsInstallerException	No such extension was found.
-	 */
-	public static ExtensionConfig findExtension(String extensionId, Map<String, ExtensionConfig> extensionConfigs)
-			throws ExtensionsInstallerException {
-		ExtensionConfig extensionConfig = extensionConfigs.get(extensionId);
-		if (extensionConfig != null) {
-			return extensionConfig;
-		}
-		throw new ExtensionsInstallerException(
-				String.format("Configuration for extension: %s was not found.", extensionId));
-	}
+    /**
+     * Returns the extension which has the given id, from the given map of extension configurations.
+     *
+     * @param extensionId					Id of the extension.
+     * @param extensionConfigs				Configurations of all available extensions.
+     * @return								Configuration of the found extension.
+     * @throws ExtensionsInstallerException	No such extension was found.
+     */
+    public static ExtensionConfig findExtension(String extensionId, Map<String, ExtensionConfig> extensionConfigs)
+            throws ExtensionsInstallerException {
+        ExtensionConfig extensionConfig = extensionConfigs.get(extensionId);
+        if (extensionConfig != null) {
+            return extensionConfig;
+        }
+        throw new ExtensionsInstallerException(
+                String.format("Configuration for extension: %s was not found.", extensionId));
+    }
 
-	/**
-	 * Returns the installation status of an extension, based on the given counts of its installed dependencies.
-	 *
-	 * @param installedDependenciesCount	Actual count of successfully installed dependencies.
-	 * @param expectedDependenciesCount		Expected count of successfully installed dependencies.
-	 * @return								Installation status of the extension.
-	 */
-	public static ExtensionInstallationStatus getInstallationStatus(int installedDependenciesCount,
-																	int expectedDependenciesCount) {
-		if (installedDependenciesCount == 0) {
-			return ExtensionInstallationStatus.NOT_INSTALLED;
-		}
-		if (installedDependenciesCount < expectedDependenciesCount) {
-			return ExtensionInstallationStatus.PARTIALLY_INSTALLED;
-		}
-		return ExtensionInstallationStatus.INSTALLED;
-	}
+    /**
+     * Returns the installation status of an extension, based on the given counts of its installed dependencies.
+     *
+     * @param installedDependenciesCount	Actual count of successfully installed dependencies.
+     * @param expectedDependenciesCount		Expected count of successfully installed dependencies.
+     * @return								Installation status of the extension.
+     */
+    public static ExtensionInstallationStatus getInstallationStatus(int installedDependenciesCount,
+                                                                    int expectedDependenciesCount) {
+        if (installedDependenciesCount == 0) {
+            return ExtensionInstallationStatus.NOT_INSTALLED;
+        }
+        if (installedDependenciesCount < expectedDependenciesCount) {
+            return ExtensionInstallationStatus.PARTIALLY_INSTALLED;
+        }
+        return ExtensionInstallationStatus.INSTALLED;
+    }
 
-	/**
-	 * Returns the un-installation status of an extension, based on the given counts of its un-installed dependencies.
-	 *
-	 * @param uninstalledDependenciesCount	Actual count of successfully un-installed dependencies.
-	 * @param expectedDependenciesCount		Expected count of successfully un-installed dependencies.
-	 * @return								Un-installation status of the extension.
-	 */
-	public static ExtensionUnInstallationStatus getUnInstallationStatus(int uninstalledDependenciesCount,
-																		int expectedDependenciesCount) {
-		if (uninstalledDependenciesCount == 0) {
-			return ExtensionUnInstallationStatus.NOT_UNINSTALLED;
-		}
-		if (uninstalledDependenciesCount < expectedDependenciesCount) {
-			return ExtensionUnInstallationStatus.PARTIALLY_UNINSTALLED;
-		}
-		return ExtensionUnInstallationStatus.UNINSTALLED;
-	}
+    /**
+     * Returns the un-installation status of an extension, based on the given counts of its un-installed dependencies.
+     *
+     * @param uninstalledDependenciesCount	Actual count of successfully un-installed dependencies.
+     * @param expectedDependenciesCount		Expected count of successfully un-installed dependencies.
+     * @return								Un-installation status of the extension.
+     */
+    public static ExtensionUnInstallationStatus getUnInstallationStatus(int uninstalledDependenciesCount,
+                                                                        int expectedDependenciesCount) {
+        if (uninstalledDependenciesCount == 0) {
+            return ExtensionUnInstallationStatus.NOT_UNINSTALLED;
+        }
+        if (uninstalledDependenciesCount < expectedDependenciesCount) {
+            return ExtensionUnInstallationStatus.PARTIALLY_UNINSTALLED;
+        }
+        return ExtensionUnInstallationStatus.UNINSTALLED;
+    }
 
-	/**
-	 * Returns files of which, name matches the given regex pattern, from the given directory path.
-	 *
-	 * @param regexPattern	Regex pattern for file name.
-	 * @param directoryPath	Path of the directory.
-	 * @return				List of matching files.
-	 * @throws IOException	Failure occurred while walking the file tree.
-	 */
-	public static List<Path> listMatchingFiles(String regexPattern, String directoryPath) throws IOException {
-		final String PATH_MATCHER_SYNTAX = "regex:";
-		Path path = Paths.get(directoryPath);
-		final PathMatcher matcher = FileSystems.getDefault().getPathMatcher(PATH_MATCHER_SYNTAX + regexPattern);
+    /**
+     * Returns files of which, name matches the given regex pattern, from the given directory path.
+     *
+     * @param regexPattern	Regex pattern for file name.
+     * @param directoryPath	Path of the directory.
+     * @return				List of matching files.
+     * @throws IOException	Failure occurred while walking the file tree.
+     */
+    public static List<Path> listMatchingFiles(String regexPattern, String directoryPath) throws IOException {
+        final String PATH_MATCHER_SYNTAX = "regex:";
+        Path path = Paths.get(directoryPath);
+        final PathMatcher matcher = FileSystems.getDefault().getPathMatcher(PATH_MATCHER_SYNTAX + regexPattern);
 
 		/*
 		Ideally a single file will be returned,
 		unless a jar with a different version (but with the same name) is manually placed in the directory.
 		 */
-		List<Path> matchingFiles = new ArrayList<>(1);
-		FileVisitor<Path> matcherVisitor = new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attribs) {
-				Path name = file.getFileName();
-				if (matcher.matches(name)) {
-					matchingFiles.add(file);
-				}
-				return FileVisitResult.CONTINUE;
-			}
-		};
-		Files.walkFileTree(path, matcherVisitor);
-		return matchingFiles;
-	}
+        List<Path> matchingFiles = new ArrayList<>(1);
+        FileVisitor<Path> matcherVisitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attribs) {
+                Path name = file.getFileName();
+                if (matcher.matches(name)) {
+                    matchingFiles.add(file);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        };
+        Files.walkFileTree(path, matcherVisitor);
+        return matchingFiles;
+    }
 
-	/**
-	 * Returns the directory path where the jar should be put to, for the given usage of a dependency.
-	 *
-	 * @param usage							Configuration of a dependency's usage.
-	 * @return								Directory path of the given usage.
-	 * @throws ExtensionsInstallerException	Invalid usage type and/or used by type.
-	 */
-	public static String getDirectoryPathFor(UsageConfig usage)
-			throws ExtensionsInstallerException {
-		UsageType usageType = usage.getType();
-		UsedByType usedBy = usage.getUsedBy();
-		if (usageType != null && usedBy != null) {
-			if (usedBy == UsedByType.RUNTIME) {
-				if (usageType == UsageType.JAR) {
-					return ExtensionsInstallerConstants.RUNTIME_JARS_LOCATION;
-				} else if (usageType == UsageType.BUNDLE) {
-					return ExtensionsInstallerConstants.RUNTIME_BUNDLES_LOCATION;
-				}
-				throw new ExtensionsInstallerException(
-						String.format("Invalid value: %s for usage type.", usageType));
-			} else if (usedBy == UsedByType.SAMPLES) {
-				if (usageType == UsageType.JAR) {
-					return ExtensionsInstallerConstants.SAMPLES_JARS_LOCATION;
-				} else if (usageType == UsageType.BUNDLE) {
-					return ExtensionsInstallerConstants.SAMPLES_BUNDLES_LOCATION;
-				}
-				throw new ExtensionsInstallerException(
-						String.format("Invalid value: %s for dependency type.", usageType));
-			}
-		}
-		throw new ExtensionsInstallerException(String.format("Invalid value: %s for 'usedBy' property.", usage));
-	}
+    /**
+     * Returns the directory path where the jar should be put to, for the given usage of a dependency.
+     *
+     * @param usage							Configuration of a dependency's usage.
+     * @return								Directory path of the given usage.
+     * @throws ExtensionsInstallerException	Invalid usage type and/or used by type.
+     */
+    public static String getDirectoryPathFor(UsageConfig usage)
+            throws ExtensionsInstallerException {
+        UsageType usageType = usage.getType();
+        UsedByType usedBy = usage.getUsedBy();
+        if (usageType != null && usedBy != null) {
+            if (usedBy == UsedByType.RUNTIME) {
+                if (usageType == UsageType.JAR) {
+                    return ExtensionsInstallerConstants.RUNTIME_JARS_LOCATION;
+                } else if (usageType == UsageType.BUNDLE) {
+                    return ExtensionsInstallerConstants.RUNTIME_BUNDLES_LOCATION;
+                }
+                throw new ExtensionsInstallerException(
+                        String.format("Invalid value: %s for usage type.", usageType));
+            } else if (usedBy == UsedByType.SAMPLES) {
+                if (usageType == UsageType.JAR) {
+                    return ExtensionsInstallerConstants.SAMPLES_JARS_LOCATION;
+                } else if (usageType == UsageType.BUNDLE) {
+                    return ExtensionsInstallerConstants.SAMPLES_BUNDLES_LOCATION;
+                }
+                throw new ExtensionsInstallerException(
+                        String.format("Invalid value: %s for dependency type.", usageType));
+            }
+        }
+        throw new ExtensionsInstallerException(String.format("Invalid value: %s for 'usedBy' property.", usage));
+    }
 }
