a70849d1041ab69ed0fa5e6c6d0b72fefd557bd4
AnuGayan <ANu4629717GAyan>
2020-02-25 13:59:18 +0530

    Add class id to Generate Stream button

--- components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/editor/js/design-view/form-builder/forms/stream-form.js
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/editor/js/design-view/form-builder/forms/stream-form.js
@@ -41,7 +41,7 @@ define(['require', 'log', 'jquery', 'lodash', 'attribute', 'jsonValidator', 'con
             var streamObject = self.configurationData.getSiddhiAppConfig().getStream(id);
             var previousStreamObject = _.cloneDeep(streamObject);
             self.propertyDiv = $('<div class="clearfix form-min-width"><div id="streamGeneration">' +
-                '<button type="button">Generate Stream</button></div>' +
+                '<button type="button" class="btn btn-primary">Generate Stream</button></div>' +
                 '<div class = "stream-form-container"> <label> ' +
                 '<span class="mandatory-symbol"> *</span> Name </label> <input type="text" id="streamName" ' +
                 'class="clearfix name"> <label class="error-message" id="streamNameErrorMessage"> </label>' +
