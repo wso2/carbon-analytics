bb00ce9212adb064e219ca46137e1aa257b64950
anushaj <anushaj@wso2.com>
2020-03-03 14:30:03 +0530

    Improve the generate stream front end implementation

--- components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/editor/commons/js/dialog/generate-stream-dialog.js
+++ components/org.wso2.carbon.siddhi.editor.core/src/main/resources/web/editor/commons/js/dialog/generate-stream-dialog.js
@@ -18,185 +18,188 @@
 define(['require', 'lodash', 'jquery', 'log'],
     function (require, _, $, log) {
 
-        var constants = {};
-        var streamObj = {};
         var requestBody = {};
         var files;
+        var config = {};
 
-
-        constants.HTTP_GET = "GET";
-        constants.HTTP_POST = "POST";
-        constants.HTTP_PUT = "PUT";
-        constants.HTTP_DELETE = "DELETE";
-        constants.editorUrl = window.location.protocol + "//" + window.location.host + '/editor';
-        constants.streamObj = {
-            "name": "testStream",
-            "attributes": [
-                {
-                    "name": "price",
-                    "dataType": "int"
-                },
-                {
-                    "name": "name",
-                    "dataType": "string"
-                },
-                {
-                    "name": "volume",
-                    "dataType": "double"
-                }
-            ]
+        var constants = {
+            HTTP_GET: 'GET',
+            HTTP_POST: 'POST',
+            HTTP_PUT: 'PUT',
+            HTTP_DELETE: 'DELETE',
+            editorUrl: window.location.protocol + '//' + window.location.host + '/editor',
+            streamObj: {
+                "name": "testStream",
+                "attributes": [
+                    {
+                        "name": "price",
+                        "dataType": "int"
+                    },
+                    {
+                        "name": "name",
+                        "dataType": "string"
+                    },
+                    {
+                        "name": "volume",
+                        "dataType": "double"
+                    }
+                ]
+            }
         };
 
         var GenerateStreamDialog = function (options, ref, callback) {
-
             this.app = options;
             this.pathSeparator = this.app.getPathSeperator();
             this.ref = ref;
-            this.callback = callback
+            this.callback = callback;
         };
 
         GenerateStreamDialog.prototype.constructor = GenerateStreamDialog;
 
         GenerateStreamDialog.prototype.render = function () {
             var self = this;
-            var app = this.app;
             if (!_.isNil(this._generateStreamModal)) {
                 this._generateStreamModal.remove();
             }
-            var generateOptions = $('#generateStreamConfigModal')
 
-            generateOptions.find(".collapse").collapse();
+            // rename the variable
+            var generateStreamModal = $('#generateStreamConfigModal');
 
-            generateOptions.find('#idFromFile').on('click', function (e) {
+            generateStreamModal.find(".collapse").collapse();
+
+            generateStreamModal.find('#idFromFile').on('click', function (e) {
                 e.stopPropagation();
 
-                generateOptions.find('#generateFromFileContent').collapse("show");
-                generateOptions.find('#fileSelector').change(function (e) {
-                        self.handleFileSelect(e, generateOptions);
-                    }
-                );
+                generateStreamModal.find('#generateFromFileContent').collapse("show");
+                generateStreamModal.find('#fileSelector').change(function (e) {
+                    self.handleFileSelect(e, generateStreamModal);
+                });
 
-                if (!generateOptions.find('#idFromDatabase').is(":checked")) {
-                    generateOptions.find('#generateFromDBContent').collapse("hide");
+                if (!generateStreamModal.find('#idFromDatabase').is(":checked")) {
+                    generateStreamModal.find('#generateFromDBContent').collapse("hide");
                 }
             });
 
-            generateOptions.find('#idFromDatabase').on('click', function (e) {
+            generateStreamModal.find('#idFromDatabase').on('click', function (e) {
                 e.stopPropagation();
 
-                generateOptions.find('#generateFromDBContent').collapse("show");
-                if (!generateOptions.find('#idFromFile').is(":checked")) {
-                    generateOptions.find('#generateFromFileContent').collapse("hide");
+                generateStreamModal.find('#generateFromDBContent').collapse("show");
+                if (!generateStreamModal.find('#idFromFile').is(":checked")) {
+                    generateStreamModal.find('#generateFromFileContent').collapse("hide");
                 }
             });
 
-            generateOptions.find('#idInline').on('click', function (e) {
+            generateStreamModal.find('#idInline').on('click', function (e) {
                 e.stopPropagation();
 
-                generateOptions.find('#inlineContent').collapse("show");
-                if (!generateOptions.find('#idDatasource').is(":checked")) {
-                    generateOptions.find('#datasourceContent').collapse("hide");
+                generateStreamModal.find('#inlineContent').collapse("show");
+                if (!generateStreamModal.find('#idDatasource').is(":checked")) {
+                    generateStreamModal.find('#datasourceContent').collapse("hide");
                 }
             });
 
-            generateOptions.find('#idDatasource').on('click', function (e) {
+            generateStreamModal.find('#idDatasource').on('click', function (e) {
                 e.stopPropagation();
 
-                generateOptions.find('#datasourceContent').collapse("show");
-                if (!generateOptions.find('#idInline').is(":checked")) {
-                    generateOptions.find('#inlineContent').collapse("hide");
+                generateStreamModal.find('#datasourceContent').collapse("show");
+                if (!generateStreamModal.find('#idInline').is(":checked")) {
+                    generateStreamModal.find('#inlineContent').collapse("hide");
                 }
             });
 
-            generateOptions.find('button').filter('#loadInlineDbConnection').click(function (e) {
-                    self.handleLoadDbConnection(e, generateOptions, self);
-                }
-            );
-            generateOptions.find('button').filter('#loadDatasourceDbConnection').click(function (e) {
-                self.handleLoadDbConnection(e, generateOptions, self);
+            // todo
+            generateStreamModal.find('#loadInlineDbConnection').click(function (e) {
+                self.handleLoadDbConnection(e, generateStreamModal, self);
+            });
+            // todo
+            generateStreamModal.find('#loadDatasourceDbConnection').click(function (e) {
+                self.handleLoadDbConnection(e, generateStreamModal, self);
             });
 
-            generateOptions.find("button").filter("#generateButton").click(function () {
+            generateStreamModal.find("#generateButton").click(function () {
                 config = {};
                 var formData = new FormData();
-                if (generateOptions.find('#idFromFile').is(":checked")) {
+                if (generateStreamModal.find('#idFromFile').is(":checked")) {
                     requestBody = {};
-                    if (generateOptions.find("#fromCsvFile").attr("aria-expanded") === "true") {
-                        requestBody["type"] = "csv";
-                        constants.streamObj.name = generateOptions.find('#streamNameCsv')[0].value;
-                        config["streamName"] = generateOptions.find('#streamNameCsv')[0].value;
-                        config["delimiter"] = generateOptions.find('#delimiterOfCSV')[0].value;
-                        config["isHeaderExist"] = generateOptions.find('#isHeaderExists')[0].value;
+                    if (generateStreamModal.find("#fromCsvFile").attr("aria-expanded") === "true") {
+                        constants.streamObj.name = generateStreamModal.find('#streamNameCsv')[0].value;
+                        config = {
+                            streamName: generateStreamModal.find('#streamNameCsv')[0].value,
+                            delemiter: generateStreamModal.find('#delimiterOfCSV')[0].value,
+                            isHeaderExist: generateStreamModal.find('#isHeaderExists')[0].value
+                        };
+                        requestBody.type = 'csv';
                         requestBody["config"] = config;
-                    } else if (generateOptions.find("#fromJsonFile").attr("aria-expanded") === "true") {
-                        requestBody["type"] = "json";
-                        constants.streamObj.name = generateOptions.find('#streamNameJson')[0].value;
-                        config["streamName"] = generateOptions.find('#streamNameJson')[0].value;
-                        config["enclosingElement"] = generateOptions.find('#jsonEnclosingElement')[0].value;
-                        requestBody["config"] = config;
-                        formData.append("config", JSON.stringify(requestBody));
-                    } else if (generateOptions.find("#fromXmlFile").attr("aria-expanded") === "true") {
+                    } else if (generateStreamModal.find("#fromJsonFile").attr("aria-expanded") === "true") {
+                        constants.streamObj.name = generateStreamModal.find('#streamNameJson')[0].value;
+                        config = {
+                            streamName: generateStreamModal.find('#streamNameJson')[0].value,
+                            enclosingElement:generateStreamModal.find('#jsonEnclosingElement')[0].value
+                        };
+                        requestBody.type = 'json';
+                        requestBody.config = config;
+                    } else if (generateStreamModal.find("#fromXmlFile").attr("aria-expanded") === "true") {
                         requestBody["type"] = "xml";
-                        constants.streamObj.name = generateOptions.find('#streamNameXml')[0].value;
-                        config["streamName"] = generateOptions.find('#streamNameXml')[0].value;
-                        config["nameSpace"] = generateOptions.find('#nameSpaceOfXml')[0].value;
-                        config["enclosingElement"] = generateOptions.find('#enclosingElementXML')[0].value;
-                        requestBody["config"] = config;
+                        constants.streamObj.name = generateStreamModal.find('#streamNameXml')[0].value;
+                        config = {
+                            streamName: generateStreamModal.find('#streamNameXml')[0].value,
+                            nameSpace: generateStreamModal.find('#nameSpaceOfXml')[0].value,
+                            enclosingElement: generateStreamModal.find('#enclosingElementXML')[0].value
+                        };
+
+                        requestBody.type = 'xml'
+                        requestBody.config = config;
                     }
                     formData.append("config", JSON.stringify(requestBody));
                     formData.append("file", files[0]);
                     self.retrieveFileDataAttributes(formData, function (data) {
                         constants.streamObj.attributes = JSON.parse(data.attributes);
                         self.callback(constants.streamObj, self.ref);
-                        generateOptions.modal('hide');
+                        generateStreamModal.modal('hide');
                     }, function (msg) {
-                        self.errorDisplay(msg, generateOptions)
+                        self.errorDisplay(msg, generateStreamModal)
 
                     });
-                } else if (generateOptions.find('#idFromDatabase').is(":checked")) {
-                    if (generateOptions.find('#idInline').is(":checked")) {
-                        var inlineTableList = generateOptions.find('#inlineTableSelector');
-                        requestBody['tableName'] = inlineTableList[0].selectedOptions[0].label;
-                        constants.streamObj.name = generateOptions.find('#streamNameInlineDB')[0].value;
-                    } else if (generateOptions.find('#idDatasource').is(":checked")) {
-                        constants.streamObj.name = generateOptions.find('#streamNameInlineDB')[0].value;
-                        var datasourceTableSelector = generateOptions.find('#datasourceTableSelector');
-                        requestBody['tableName'] = datasourceTableSelector[0].selectedOptions[0].label;
+                } else if (generateStreamModal.find('#idFromDatabase').is(":checked")) {
+                    if (generateStreamModal.find('#idInline').is(":checked")) {
+                        var inlineTableList = generateStreamModal.find('#inlineTableSelector');
+                        requestBody.tableName = inlineTableList[0].selectedOptions[0].label;
+                        constants.streamObj.name = generateStreamModal.find('#streamNameInlineDB')[0].value;
+                    } else if (generateStreamModal.find('#idDatasource').is(":checked")) {
+                        constants.streamObj.name = generateStreamModal.find('#streamNameInlineDB')[0].value;
+                        var datasourceTableSelector = generateStreamModal.find('#datasourceTableSelector');
+                        requestBody.tableName = datasourceTableSelector[0].selectedOptions[0].label;
                     }
                     self.retrieveTableColumnNames(requestBody, function (data) {
                         constants.streamObj.attributes = data.attributes;
                         self.callback(constants.streamObj, self.ref);
-                        generateOptions.modal('hide');
+                        generateStreamModal.modal('hide');
                     }, function (err) {
-                        self.errorDisplay(err, generateOptions)
+                        self.errorDisplay(err, generateStreamModal)
                     });
                 }
             });
 
-
-            var generateStreamConfigModal = generateOptions.filter("#generateStreamConfigModal");
-            var generateStreamConfigModalError = generateOptions.find("#generateStreamConfigModalError");
+            // todo
+            var generateStreamConfigModal = generateStreamModal.filter("#generateStreamConfigModal");
+            var generateStreamConfigModalError = generateStreamModal.find("#generateStreamConfigModalError");
             generateStreamConfigModalError.hide();
-            this._generateStreamModal = generateOptions;
+            this._generateStreamModal = generateStreamModal;
             generateStreamConfigModal.modal('hide');
         };
 
         GenerateStreamDialog.prototype.handleFileSelect = function (evt, generateOptions) {
             files = evt.target.files; // FileList object
-            generateOptions.find("#fromJsonFile").collapse("hide");
-            generateOptions.find("#fromXmlFile").collapse("hide");
-            generateOptions.find("#fromCsvFile").collapse("hide");
+            generateOptions.find('.from-file-section').collapse('hide');
             var fileType = files[0].type;
 
-            if (fileType === "application/json") {
-                generateOptions.find("#fromJsonFile").collapse("show");
-            } else if (fileType === "text/xml") {
-                generateOptions.find("#fromXmlFile").collapse("show");
-            } else if (fileType === "text/csv") {
-                generateOptions.find("#fromCsvFile").collapse("show");
+            var section = generateOptions.find('.from-file-section[data-file-type=' + fileType + ']');
+            if (section) {
+                section.collapse('show');
             } else {
                 self.alertError("Error Occurred while processing the file. File type does not supported")
             }
+
             var output = [];
             for (var i = 0, f; f = files[i]; i++) {
                 output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
@@ -210,21 +213,23 @@ define(['require', 'lodash', 'jquery', 'log'],
         GenerateStreamDialog.prototype.handleLoadDbConnection = function (evt, generateOptions, self) {
             requestBody = {};
             if (generateOptions.find("#inlineContent").attr("aria-expanded") === "true") {
-                requestBody['url'] = generateOptions.find('#dataSourceLocation_1')[0].value;
-                requestBody['username'] = generateOptions.find('#inlineUsername')[0].value;
-                requestBody['password'] = generateOptions.find('#inlinePass')[0].value;
+                requestBody = {
+                    url: generateOptions.find('#dataSourceLocation_1')[0].value,
+                    username: generateOptions.find('#inlineUsername')[0].value,
+                    password: generateOptions.find('#inlinePass')[0].value
+                };
             } else if (generateOptions.find("#datasourceContent").attr("aria-expanded") === "true") {
-                requestBody['dataSourceName'] = generateOptions.find('#dataSourceNameId')[0].value;
+                requestBody.dataSourceName = generateOptions.find('#dataSourceNameId')[0].value;
             }
             console.log(requestBody);
             self.connectToDatabase(requestBody, function (evt) {
                 self.retrieveTableNames(requestBody, function (evt) {
                     self.populateInlineTableList(evt.tables, generateOptions);
                 }, function (err) {
-                    self.alertError(err.error);
+                    alertError(err.error);
                 });
             }, function (err) {
-                self.alertError(err.error);
+                alertError(err.error);
             });
         };
 
@@ -233,10 +238,10 @@ define(['require', 'lodash', 'jquery', 'log'],
         };
         GenerateStreamDialog.prototype.populateInlineTableList = function (data, generateOptions) {
             if (data != null && data instanceof Array) {
-                  data.forEach( function (value, index) {
-                      generateOptions.find('.tableSelector').append('<option value=' + index + '>'
-                          + value + '</option>');
-                  });
+                data.forEach(function (value, index) {
+                    generateOptions.find('.tableSelector').append('<option value=' + index + '>'
+                        + value + '</option>');
+                });
             }
         };
 
@@ -286,7 +291,7 @@ define(['require', 'lodash', 'jquery', 'log'],
                     async: false,
                     url: constants.editorUrl + "/retrieveTableColumnNames",
                     type: constants.HTTP_POST,
-                    contentType: "application/json; charset=utf-8",
+                    contentType: "application/json; charset=utf-8", // todo constant
                     data: JSON.stringify(connectionDetails),
                     success: function (data) {
                         if (typeof successCallback === 'function')
