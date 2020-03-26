93cfc78a1d2783064b3f15ebab336e528e72d367
anushaj <anushaj@wso2.com>
2020-03-26 14:07:00 +0530

    Update the carbon-analytics 2.2.x branch

--- components/org.wso2.carbon.business.rules.web/README.md
+++
@@ -1,1 +0,0 @@
-## Business Rules Manager web component
--- components/org.wso2.carbon.streaming.integrator.common/src/main/java/org/wso2/carbon/streaming/integrator/common/SiddhiAppDeployerReceiver.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.streaming.integrator.common;
-
-/**
- * SiddhiAppDeployerReceiver is used to notify when new execution plans are deployed
- */
-public interface SiddhiAppDeployerReceiver {
-}
